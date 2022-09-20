0cb943edff0d973e5c9d2073ec857613ba0d4fb8
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2022-09-20 16:26:34 +0900

    [feature/flipper_config] Flipper 설정 (#6)
    * [feature/flipper_config] Flipper 의존성 설정
    
    * [feature/flipper_config] Flipper 셋팅 및 추가 의존성 설정

---
+++ app/src/main/java/org/sopt/official/App.kt
@@ -0,0 +1,30 @@
+package org.sopt.official
+
+import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initFlipper()
+    }
+
+    private fun initFlipper() {
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            AndroidFlipperClient.getInstance(this).apply {
+                addPlugin(InspectorFlipperPlugin(this@App, DescriptorMapping.withDefaults()))
+                addPlugin(networkFlipperPlugin)
+            }.start()
+        }
+    }
+
+    companion object {
+        val networkFlipperPlugin = NetworkFlipperPlugin()
+    }
+}
