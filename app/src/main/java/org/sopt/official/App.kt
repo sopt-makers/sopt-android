024561fff818361bca65105f07536ee4e939039d
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-09-21 19:25:18 +0900

    [feature/hilt-config] HiltAndroidApp 달기

--- app/src/main/java/org/sopt/official/App.kt
+++ app/src/main/java/org/sopt/official/App.kt
@@ -7,7 +7,9 @@ import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
 import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import com.facebook.soloader.SoLoader
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
