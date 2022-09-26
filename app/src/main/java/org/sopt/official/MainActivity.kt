bb60534f44ac27a08432dcebe71ede9fb6704515
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-09-26 23:03:28 +0900

    [feature/crashlytics-register] Crashlytics 의존성 설정 추가

--- app/src/main/java/org/sopt/official/MainActivity.kt
+++ app/src/main/java/org/sopt/official/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
