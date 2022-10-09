adc8279ee96ed014cf3ba854bc6c1b16695d1cca
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2022-10-09 18:19:04 +0900

    [feature/theme-config-compose] Compose 테마/스타일링 설정 (#18)
    * style: Color 설정
    
    feature/theme-config-compose
    
    * style: Type 설정
    
    feature/theme-config-compose
    
    * style: Theme 설정
    
    feature/theme-config-compose

---
+++ app/src/main/java/org/sopt/official/style/Color.kt
@@ -0,0 +1,25 @@
+package org.sopt.official.style
+
+import androidx.compose.ui.graphics.Color
+
+// Blue Scale
+val Blue600 = Color(0xFF2E60F6)
+val Blue500 = Color(0xFF5070F7)
+val Blue400 = Color(0xFF6380FC)
+val Blue300 = Color(0xFF7F98FF)
+val Blue200 = Color(0xFFA1B3FF)
+val Blue100 = Color(0xFFC1CDFF)
+
+// Gray Scale
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray900 = Color(0xFF151617)
+val Gray800 = Color(0xFF353637)
+val Gray700 = Color(0xFF535455)
+val Gray600 = Color(0xFF666768)
+val Gray500 = Color(0xFF8E8F90)
+val Gray400 = Color(0xFFAEAFB0)
+val Gray300 = Color(0xFFD3D4D5)
+val Gray200 = Color(0xFFE4E5E6)
+val Gray100 = Color(0xFFEFF0F1)
+val Gray50 = Color(0xFFF7F8F9)
