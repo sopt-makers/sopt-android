eec82bafb8b88d750a12c9990ccdbf839c406e1e
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2022-10-10 19:34:49 +0900

    [feature/fix-build-error] 의존성 주입 + Compose Version 관련 에러 수정 (#20)
    * chore: Compose Compiler 버전 업
    
    1.2.1 -> 1.3.2
    
    * fix: UpdateModule에서 AppUpdateManager 주입 안해줘서 발생하는 오류 해결

--- app/src/main/java/org/sopt/official/di/UpdateModule.kt
+++ app/src/main/java/org/sopt/official/di/UpdateModule.kt
@@ -1,9 +1,13 @@
 package org.sopt.official.di
 
+import android.content.Context
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.scopes.ActivityScoped
 import org.sopt.official.feature.update.InAppUpdateManager
 import org.sopt.official.feature.update.InAppUpdateManagerImpl
@@ -13,7 +17,13 @@ import org.sopt.official.feature.update.InAppUpdateManagerImpl
 object UpdateModule {
     @Provides
     @ActivityScoped
-    fun provideAppUpdateManager(
+    fun provideInAppUpdateManager(
         updateManager: InAppUpdateManagerImpl
     ): InAppUpdateManager = updateManager
+
+    @Provides
+    @ActivityScoped
+    fun provideAppUpdateManager(
+        @ActivityContext context: Context
+    ): AppUpdateManager = AppUpdateManagerFactory.create(context)
 }
