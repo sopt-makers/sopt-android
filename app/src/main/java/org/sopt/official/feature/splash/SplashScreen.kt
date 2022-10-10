51d9fe57cdd757bbca6361e2cde8d0b04c9639e2
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-10-10 19:31:25 +0900

    feat: Splash Screen SOPT 텍스트 애니메이션 설정
    feature/splash-screen

--- app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
+++ app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
@@ -1,16 +1,24 @@
 package org.sopt.official.feature.splash
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.sopt.official.style.SoptTheme
-import org.sopt.official.style.SoptTypography
 
 @Composable
 fun SplashScreen() {
@@ -20,14 +28,42 @@ fun SplashScreen() {
                 .fillMaxSize()
                 .background(SoptTheme.colors.onSurface90)
         ) {
-            Text(
-                text = "Hello SOPT",
-                style = SoptTheme.typography.h1,
-                color = SoptTheme.colors.background,
-                modifier = Modifier
-                    .wrapContentSize()
-                    .align(Alignment.Center)
-            )
+            val textAnimationState = remember {
+                MutableTransitionState(false)
+                    .apply { targetState = true }
+            }
+            var isTitleAnimationFinished by remember(textAnimationState.currentState) {
+                mutableStateOf(false)
+            }
+            val remainedAnimationState = remember(isTitleAnimationFinished) {
+                MutableTransitionState(isTitleAnimationFinished)
+                    .apply { targetState = true }
+            }
+            val density = LocalDensity.current
+
+            LaunchedEffect(textAnimationState.currentState) {
+                if (textAnimationState.currentState) isTitleAnimationFinished = true
+            }
+
+            AnimatedVisibility(
+                visibleState = textAnimationState,
+                modifier = Modifier.align(Alignment.Center),
+                enter = slideInHorizontally(tween(800)) {
+                    with(density) { -20.dp.roundToPx() }
+                } + fadeIn(
+                    animationSpec = tween(800, easing = FastOutSlowInEasing),
+                    initialAlpha = 0.1f
+                ),
+            ) {
+                Text(
+                    text = "SOPT",
+                    style = SoptTheme.typography.h1,
+                    color = SoptTheme.colors.background,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.Center)
+                )
+            }
         }
     }
 }
