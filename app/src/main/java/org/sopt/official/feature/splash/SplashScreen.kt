9814e9533f4ecdc02a2a12e958c110b33ec9cf43
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-10-13 13:33:53 +0900

    feat: 로고 및 점, 텍스트 추가

--- app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
+++ app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
@@ -7,16 +7,18 @@ import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.style.SoptTheme
@@ -27,65 +29,106 @@ fun SplashScreen() {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(SoptTheme.colors.onSurface90)
+                .background(SoptTheme.colors.onSurface90),
+            contentAlignment = Alignment.Center
         ) {
             val textAnimationState = remember {
                 MutableTransitionState(false)
                     .apply { targetState = true }
             }
             var isTitleAnimationFinished by remember(textAnimationState.currentState) {
+                Log.d("Nunu", "isTitleAnimationFinished: ${textAnimationState.currentState}")
                 mutableStateOf(false)
             }
             val remainedAnimationState = remember(isTitleAnimationFinished) {
                 MutableTransitionState(isTitleAnimationFinished)
                     .apply { targetState = true }
             }
-            val density = LocalDensity.current
 
             LaunchedEffect(textAnimationState.currentState) {
                 if (textAnimationState.currentState) isTitleAnimationFinished = true
             }
 
-            Column(
-                modifier = Modifier.align(Alignment.Center)
+            Title(textAnimationState, remainedAnimationState)
+        }
+    }
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+private fun Title(
+    titleAnimationState: MutableTransitionState<Boolean>,
+    remainedAnimationState: MutableTransitionState<Boolean>
+) {
+    val density = LocalDensity.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            AnimatedVisibility(
+                visibleState = titleAnimationState,
+                enter = slideInHorizontally(tween(800)) {
+                    with(density) { -20.dp.roundToPx() }
+                } + fadeIn(
+                    animationSpec = tween(800, easing = FastOutSlowInEasing),
+                    initialAlpha = 0.1f
+                ),
             ) {
-                AnimatedVisibility(
-                    visibleState = textAnimationState,
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    enter = slideInHorizontally(tween(800)) {
-                        with(density) { -20.dp.roundToPx() }
-                    } + fadeIn(
-                        animationSpec = tween(800, easing = FastOutSlowInEasing),
-                        initialAlpha = 0.1f
-                    ),
-                ) {
-                    Text(
-                        text = "SOPT",
-                        style = SoptTheme.typography.h1,
-                        color = SoptTheme.colors.background,
-                        modifier = Modifier
-                            .wrapContentSize()
+                Text(
+                    text = "SOPT",
+                    style = SoptTheme.typography.h1,
+                    color = SoptTheme.colors.background,
+                    modifier = Modifier.wrapContentSize(),
+                    platformStyle = PlatformTextStyle(
+                        includeFontPadding = false
                     )
-                }
-                AnimatedVisibility(
-                    visibleState = textAnimationState,
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    enter = slideInHorizontally(tween(800)) {
-                        with(density) { 20.dp.roundToPx() }
-                    } + fadeIn(
-                        animationSpec = tween(800, easing = FastOutSlowInEasing),
-                        initialAlpha = 0.1f
-                    ),
+                )
+            }
+            AnimatedVisibility(
+                visibleState = remainedAnimationState,
+                enter = slideInHorizontally(tween(800)) {
+                    with(density) { -20.dp.roundToPx() }
+                } + fadeIn(
+                    animationSpec = tween(800, easing = FastOutSlowInEasing),
+                    initialAlpha = 0.1f
+                ),
+            ) {
+                Canvas(
+                    modifier = Modifier
+                        .size(6.dp)
+                        .align(Alignment.Bottom)
                 ) {
-                    Text(
-                        text = "Shout Our Passion Together",
-                        style = SoptTheme.typography.b2,
-                        color = SoptTheme.colors.onSurface50,
-                        modifier = Modifier
-                            .wrapContentSize()
+                    drawRect(
+                        color = Color(0xFF5070F7),
+                        size = Size(size.width, size.height)
                     )
                 }
             }
+        }
+
+        AnimatedVisibility(
+            visibleState = remainedAnimationState,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            enter = slideInHorizontally(tween(800)) {
+                with(density) { 20.dp.roundToPx() }
+            } + fadeIn(
+                animationSpec = tween(800, easing = FastOutSlowInEasing),
+                initialAlpha = 0.1f
+            ),
+        ) {
+            Text(
+                text = "Shout Our Passion Together",
+                style = SoptTheme.typography.b2,
+                color = SoptTheme.colors.onSurface50,
+                modifier = Modifier
+                    .wrapContentSize()
+            )
         }
     }
 }
