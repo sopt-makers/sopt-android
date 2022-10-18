e3f8d6854a6da8090e08d3fc43567067fde5bdaa
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-10-18 18:13:18 +0900

    fix: 안쓰는 import 삭제
    feature/splash-screen

--- app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
+++ app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
@@ -1,6 +1,5 @@
 package org.sopt.official.feature.splash
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.MutableTransitionState
@@ -15,10 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.style.Blue500
