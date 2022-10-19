49c01f1b402caac9d160cd755eebe0b257db4aca
Seran Park <59532818+sery270@users.noreply.github.com>
2022-10-19 15:10:43 +0900

    [Feature/lint setting] ktlint 세팅 (#29)
    * feat: ktlint 세팅
    
    * feat: ktlint 수정
    
    * chore: 하드코딩된 디펜던시 관리

--- app/src/main/java/org/sopt/official/feature/update/UpdateCriteria.kt
+++ app/src/main/java/org/sopt/official/feature/update/UpdateCriteria.kt
@@ -7,27 +7,23 @@ enum class UpdateCriteria(
     val stalenessDaysOfImmediateUpdate: Int?,
     val stalenessDaysOfFlexibleUpdate: Int?
 ) {
-    URGENT(5, null, null),
-    SLIGHTLY_URGENT(4, 5, 3),
-    NORMAL(3, 30, 15),
-    SLIGHTLY_FLEXIBLE(2, 90, 30),
-    FLEXIBLE(1, null, null);
+    URGENT(5, null, null), SLIGHTLY_URGENT(4, 5, 3), NORMAL(3, 30, 15), SLIGHTLY_FLEXIBLE(2, 90, 30), FLEXIBLE(1, null, null);
 
     companion object {
         private fun of(priority: Int): UpdateCriteria {
-            return values().find { it.priority == priority }
-                ?: throw IllegalStateException("Unknown priority: $priority")
+            return values().find { it.priority == priority } ?: throw IllegalStateException("Unknown priority: $priority")
         }
 
-        private fun UpdateCriteria.stalenessDayOf(type: Int) =
-            if (type == AppUpdateType.IMMEDIATE) {
-                stalenessDaysOfImmediateUpdate!!
-            } else {
-                stalenessDaysOfFlexibleUpdate!!
-            }
+        private fun UpdateCriteria.stalenessDayOf(type: Int) = if (type == AppUpdateType.IMMEDIATE) {
+            stalenessDaysOfImmediateUpdate!!
+        } else {
+            stalenessDaysOfFlexibleUpdate!!
+        }
 
         fun isUpdatableOf(
-            priority: Int, stalenessDays: Int?, updateType: Int
+            priority: Int,
+            stalenessDays: Int?,
+            updateType: Int
         ): Boolean {
             return when (priority) {
                 5 -> true
