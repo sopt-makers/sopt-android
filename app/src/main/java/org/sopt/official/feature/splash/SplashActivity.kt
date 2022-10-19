ea4bdf870790ed24bb1b86dd1e7464d77a8b9984
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2022-10-19 11:20:33 +0900

    [feature/splash-screen] Splash 화면 구현 (#19)
    * chore: navigation-compose 설정
    
    feature/splash-screen
    
    * style: Splash window background 속성 추가
    
    * feature: SplashActivity & Screen 기초 세팅
    
    * feat: Splash Screen SOPT 텍스트 애니메이션 설정
    
    feature/splash-screen
    
    * feat: Splash Screen SubText 텍스트 애니메이션 설정
    
    feature/splash-screen
    
    * feat: 로고 및 점, 텍스트 추가
    
    * style: Canvas 내에 Blue500으로 색 교체
    
    feature/splash-screen
    
    * fix: 안쓰는 import 삭제
    
    feature/splash-screen
    
    * fix: 스플래시 급한 불 끄기
    
    feature/splash-screen

---
+++ app/src/main/java/org/sopt/official/feature/splash/SplashActivity.kt
@@ -0,0 +1,16 @@
+package org.sopt.official.feature.splash
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen()
+        }
+    }
+}
---
+++ app/src/main/java/org/sopt/official/feature/splash/SplashScreen.kt
@@ -0,0 +1,139 @@
+package org.sopt.official.feature.splash
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.official.style.Blue500
+import org.sopt.official.style.SoptTheme
+
+@Composable
+fun SplashScreen() {
+    SoptTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(SoptTheme.colors.onSurface90),
+            contentAlignment = Alignment.Center
+        ) {
+            val textAnimationState = remember {
+                MutableTransitionState(false)
+                    .apply { targetState = true }
+            }
+            var isTitleAnimationFinished by remember(textAnimationState.currentState) {
+                mutableStateOf(false)
+            }
+            val remainedAnimationState = remember(isTitleAnimationFinished) {
+                MutableTransitionState(false)
+                    .apply { targetState = isTitleAnimationFinished }
+            }
+
+            LaunchedEffect(textAnimationState.currentState) {
+                if (textAnimationState.currentState) isTitleAnimationFinished = true
+            }
+
+            Title(textAnimationState, remainedAnimationState)
+        }
+    }
+}
+
+@Composable
+private fun Title(
+    titleAnimationState: MutableTransitionState<Boolean>,
+    remainedAnimationState: MutableTransitionState<Boolean>
+) {
+    val density = LocalDensity.current
+
+    Column(
+        modifier = Modifier
+            .wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            AnimatedVisibility(
+                visibleState = titleAnimationState,
+                enter = slideInHorizontally(tween(800)) {
+                    with(density) { -20.dp.roundToPx() }
+                } + fadeIn(
+                    animationSpec = tween(800, easing = FastOutSlowInEasing),
+                    initialAlpha = 0.1f
+                ),
+            ) {
+                Text(
+                    text = "SOPT",
+                    style = SoptTheme.typography.h1,
+                    color = SoptTheme.colors.background,
+                    modifier = Modifier.wrapContentSize(),
+                )
+            }
+            AnimatedVisibility(
+                visibleState = titleAnimationState,
+                enter = slideInHorizontally(tween(800)) {
+                    with(density) { -20.dp.roundToPx() }
+                } + fadeIn(
+                    animationSpec = tween(800, easing = FastOutSlowInEasing),
+                    initialAlpha = 0.1f
+                ),
+                modifier = Modifier.offset(y = (-4).dp)
+            ) {
+                Canvas(
+                    modifier = Modifier
+                        .size(6.dp)
+                ) {
+                    drawRect(
+                        color = Blue500,
+                        size = Size(size.width, size.height)
+                    )
+                }
+            }
+        }
+        if (titleAnimationState.isIdle && titleAnimationState.currentState) {
+            AnimatedVisibility(
+                visibleState = remainedAnimationState,
+                enter = slideInHorizontally(tween(800)) {
+                    with(density) { 20.dp.roundToPx() }
+                } + fadeIn(
+                    animationSpec = tween(800, easing = FastOutSlowInEasing),
+                    initialAlpha = 0.1f
+                ),
+                modifier = Modifier.wrapContentSize()
+            ) {
+                Text(
+                    text = "Shout Our Passion Together",
+                    style = SoptTheme.typography.b2,
+                    color = SoptTheme.colors.onSurface50,
+                    modifier = Modifier.wrapContentSize()
+                )
+            }
+        } else {
+            Text(
+                text = "Shout Our Passion Together",
+                style = SoptTheme.typography.b2,
+                color = SoptTheme.colors.onSurface90,
+                modifier = Modifier.wrapContentSize()
+            )
+        }
+    }
+}
+
+@Preview("Splash 화면")
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
