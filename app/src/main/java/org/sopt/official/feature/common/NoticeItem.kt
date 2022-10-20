6b40e69f8561610e5c95173c2b4d3cb89007b035
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-21 06:33:54 +0900

    feat: NoticeItem 틀 추가
    feature/ui-notice

--- app/src/main/java/org/sopt/official/feature/common/NoticeItem.kt
+++ app/src/main/java/org/sopt/official/feature/common/NoticeItem.kt
@@ -1,11 +1,26 @@
 package org.sopt.official.feature.common
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.sopt.official.style.SoptTheme
 
 @Composable
 fun NoticeItem() {
+    Box(
+        modifier = Modifier.fillMaxWidth()
+            .padding(top = 16.dp, bottom = 14.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+        }
+    }
 }
 
 @Preview(showBackground = true)
