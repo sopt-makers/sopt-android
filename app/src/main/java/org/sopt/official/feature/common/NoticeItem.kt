90c4bfae5bbcf6babe97789d59cec30f8f362faa
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-21 06:31:10 +0900

    feat: Notice Item 파일 생성
    공지 아이템에서 1주 이내에 올라온 공지인 경우 이를 표시하기 위해 이미지를 추가함
    
    feature/ui-notice

---
+++ app/src/main/java/org/sopt/official/feature/common/NoticeItem.kt
@@ -0,0 +1,17 @@
+package org.sopt.official.feature.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.official.style.SoptTheme
+
+@Composable
+fun NoticeItem() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewNoticeItem() {
+    SoptTheme {
+        NoticeItem()
+    }
+}
