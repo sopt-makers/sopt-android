302822ad01575f289642ee532ecb14c9fbb9996a
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-21 06:37:44 +0900

    feat: NoticeItem 제목 추가
    isNewNotice 조건인 1주 이내 올라온 새로운 공지인 경우는 제목 앞에 새로운 공지라는 아이콘을 보여주는 제목 부분 작성
    
    feature/ui-notice

--- app/src/main/java/org/sopt/official/feature/common/NoticeItem.kt
+++ app/src/main/java/org/sopt/official/feature/common/NoticeItem.kt
@@ -1,13 +1,23 @@
 package org.sopt.official.feature.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.sopt.official.R
 import org.sopt.official.style.SoptTheme
 
 @Composable
@@ -20,6 +30,31 @@ fun NoticeItem() {
             modifier = Modifier.fillMaxWidth()
         ) {
         }
+    }
+}
+
+@Composable
+private fun NoticeItemTitle(
+    title: String,
+    isNewNotice: Boolean
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (isNewNotice) {
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.icon_new),
+                contentDescription = "new content icon"
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+        }
+        Text(
+            text = title,
+            style = SoptTheme.typography.sub2,
+            color = SoptTheme.colors.onSurface90,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
