3e40dcd492ad5ade1fbb51dc3e814449d7c6b8d8
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-23 19:31:55 +0900

    feat: Create TopBarIconButton component
    hotfix/package

---
+++ app/src/main/java/org/sopt/official/designsystem/components/TopBarIconButton.kt
@@ -0,0 +1,19 @@
+package org.sopt.official.designsystem.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.official.designsystem.style.SoptTheme
+
+@Composable
+fun TopBarIconButton() {
+    Row() {}
+}
+
+@Preview
+@Composable
+fun PreviewTopBarIconButton() {
+    SoptTheme {
+        TopBarIconButton()
+    }
+}
