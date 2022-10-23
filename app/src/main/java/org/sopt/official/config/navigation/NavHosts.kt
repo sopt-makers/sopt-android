7b332c9b6cfac555c31073453212415c487ab979
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-10-23 16:53:27 +0900

    NavHost 설정

---
+++ app/src/main/java/org/sopt/official/config/navigation/NavHosts.kt
@@ -0,0 +1,16 @@
+package org.sopt.official.config.navigation
+
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+
+@RootNavGraph(start = true)
+@NavGraph
+annotation class AuthNavGraph(
+    val start: Boolean = false
+)
+
+@RootNavGraph
+@NavGraph
+annotation class HomeNavGraph(
+    val start: Boolean = false
+)
