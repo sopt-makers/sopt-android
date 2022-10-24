8bd1011c16b8fa24ed5e6f0cc955d6e46ab7b0db
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-24 23:05:40 +0900

    feat: TopBarIconButton 작성
    feature/ui-notice-detail

--- app/src/main/java/org/sopt/official/designsystem/components/TopBarIconButton.kt
+++ app/src/main/java/org/sopt/official/designsystem/components/TopBarIconButton.kt
@@ -1,16 +1,31 @@
 package org.sopt.official.designsystem.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.official.R
 import org.sopt.official.designsystem.style.SoptTheme
 
 @Composable
 fun TopBarIconButton() {
-    Row() {}
+    Row(
+        modifier = Modifier.clickable { /*TODO*/ }
+    ) {
+        Image(
+            modifier = Modifier.padding(8.dp),
+            painter = painterResource(id = R.drawable.ic_back),
+            contentDescription = "Back button of Notice detail screen"
+        )
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewTopBarIconButton() {
     SoptTheme {
