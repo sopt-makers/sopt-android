bff5435278971d443effb7dcfe2567375c9ad6ec
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-27 04:32:29 +0900

    feat: TopBarIconButton Component 제작
    imageVector, tint, onClick 옵션을 사용하여 IconButton의 기본 padding 을 제거한 Button Component 제작
    
    feature/ui-notice-detail

--- app/src/main/java/org/sopt/official/designsystem/components/TopBarIconButton.kt
+++ app/src/main/java/org/sopt/official/designsystem/components/TopBarIconButton.kt
@@ -1,26 +1,50 @@
 package org.sopt.official.designsystem.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import org.sopt.official.R
 import org.sopt.official.designsystem.style.SoptTheme
 
+/**
+ * TopBar 에서 사용될 Icon Button 입니다.
+ * Icon에 들어갈 ImageVector 는 24*24로 통일된 크기를 가정합니다.
+ * 기존 IconButton 에는 내부적인 padding 이 있어 이를 대체하여 작성하였습니다.
+ *
+ * @param imageVector [ImageVector] to draw inside this Icon
+ *
+ * @param contentDescription text used by accessibility services to describe what this icon
+ * represents. This should always be provided unless this icon is used for decorative purposes,
+ * and does not represent a meaningful action that a user can take. This text should be
+ * localized, such as by using [androidx.compose.ui.res.stringResource] or similar
+ *
+ * @param tint tint to be applied to [imageVector]. If [Color.Unspecified] is provided, then no
+ *  tint is applied
+ *
+ * @author jinsu4755
+ * */
 @Composable
-fun TopBarIconButton() {
-    Row(
-        modifier = Modifier.clickable { /*TODO*/ }
+fun TopBarIconButton(
+    imageVector: ImageVector,
+    contentDescription: String? = null,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
+    onClick: (() -> Unit)? = null
+) {
+    Box(
+        modifier = Modifier.clickable { onClick?.invoke() }
     ) {
-        Image(
-            modifier = Modifier.padding(8.dp),
-            painter = painterResource(id = R.drawable.ic_back),
-            contentDescription = "Back button of Notice detail screen"
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            tint = tint
         )
     }
 }
@@ -29,6 +53,8 @@ fun TopBarIconButton() {
 @Composable
 fun PreviewTopBarIconButton() {
     SoptTheme {
-        TopBarIconButton()
+        TopBarIconButton(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_back)
+        )
     }
 }
