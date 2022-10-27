e2c6c35ecdd95295bd6b6caf38ecb2b8923cc6ba
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-28 03:28:17 +0900

    feat: 공지 상세뷰 파일 생성
    feature/ui-notice-detail

---
+++ app/src/main/java/org/sopt/official/feature/notice/NoticeDetailScreen.kt
@@ -0,0 +1,17 @@
+package org.sopt.official.feature.notice
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.official.designsystem.style.SoptTheme
+
+@Composable
+fun NoticeDetailScreen() {
+}
+
+@Preview
+@Composable
+fun PreviewNoticeDetailScreen() {
+    SoptTheme {
+        NoticeDetailScreen()
+    }
+}
