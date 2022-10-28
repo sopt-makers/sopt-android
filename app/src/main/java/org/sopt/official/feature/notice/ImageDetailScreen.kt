b262a13426be593648c6dd43d5f07ea24731d849
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-28 09:08:57 +0900

    feat: 공지 디테일화면 이미지 상세보기 화면 생성
    feature/ui-notice-detail

---
+++ app/src/main/java/org/sopt/official/feature/notice/ImageDetailScreen.kt
@@ -0,0 +1,17 @@
+package org.sopt.official.feature.notice
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.official.designsystem.style.SoptTheme
+
+@Composable
+fun ImageDetailScreen() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewImageDetailScreen() {
+    SoptTheme {
+        ImageDetailScreen()
+    }
+}
