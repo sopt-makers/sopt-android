f283d8fc2048ecf05a1a77c61f7441bc69a5781b
jinsu4755 <parkjinsu4755@gmail.com>
2022-10-28 09:00:55 +0900

    feat: 리플효과 없는 clickable 유틸함수 작성
    feature/ui-notice-detail

---
+++ app/src/main/java/org/sopt/official/utils/NoRippleClickable.kt
@@ -0,0 +1,16 @@
+package org.sopt.official.utils
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() }
+    ) {
+        onClick()
+    }
+}
