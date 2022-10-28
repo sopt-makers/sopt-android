ab9a4745e03ebad2c6d40d56993aa424e173d9e6
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-10-28 23:23:51 +0900

    feat: 체크박스 구현
    feature/push-alarm-config

--- app/src/main/java/org/sopt/official/domain/entity/Part.kt
+++ app/src/main/java/org/sopt/official/domain/entity/Part.kt
@@ -1,14 +1,16 @@
 package org.sopt.official.domain.entity
 
 enum class Part(
-    val title: String
+    val title: String,
+    val soptTitle: String,
 ) {
-    PLAN("plan"),
-    DESIGN("design"),
-    WEB("web"),
-    ANDROID("android"),
-    IOS("ios"),
-    SERVER("server");
+    ALl("all", "전체 공지"),
+    PLAN("plan", "기획"),
+    DESIGN("design", "디자인"),
+    WEB("web", "Web"),
+    ANDROID("android", "Android"),
+    IOS("ios", "iOS"),
+    SERVER("server", "Server");
 
     companion object {
         fun of(title: String): Part {
