82f3bf982b2518d478bcd0ba4876491ef83e7ceb
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-03-10 19:44:47 +0900

    chore: Mavericks Config (#28)
    feature/plant-timber

--- app/src/main/java/org/sopt/official/App.kt
+++ app/src/main/java/org/sopt/official/App.kt
@@ -2,6 +2,7 @@ package org.sopt.official
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.airbnb.mvrx.mocking.MockableMavericks
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
@@ -15,7 +16,12 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initFlipper()
+        initMavericks()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun initMavericks() {
+        MockableMavericks.initialize(this)
     }
 
     private fun initFlipper() {
