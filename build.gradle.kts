149d276b1f2469151ff44a6d3d51a47c9f792d33
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-03-23 22:25:10 +0900

    :recycle: Groovy -> kts 파일 마이그레이션 (#119)
    * :recycle: Groovy -> kts 파일 마이그레이션
    
    * :rotating_light: 규칙 완화

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath(libs.kotlin.gradleplugin)
+        classpath(libs.hilt.plugin)
+        classpath(libs.agp)
+        classpath(libs.crashlytics.plugin)
+        classpath(libs.ktlint)
+        classpath(libs.secret.plugin)
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
+    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.secret) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,4 +14,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "SOPT"
-include ':app'
+include(":app")
