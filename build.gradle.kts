983e314f6656bc18bae103578384e3459418b6f3
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-03-24 23:08:23 +0900

    :technologist: #123 Sentry Configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.secret) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.sentry) apply false
 }
 
 tasks.register("clean", Delete::class) {
