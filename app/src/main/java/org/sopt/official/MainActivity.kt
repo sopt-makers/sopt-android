ce9833025559a876119e77302cd4670f7c3335a8
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-03-24 17:59:02 +0900

    [feature/#117] FCM 설정 (#122)
    * :heavy_plus_sign: Add firebase, coroutines deps
    
    * 메인화면에 토큰 가져오는 로직과 토큰 재생성 로직 추가

--- app/src/main/java/org/sopt/official/MainActivity.kt
+++ app/src/main/java/org/sopt/official/MainActivity.kt
@@ -1,29 +1,72 @@
 package org.sopt.official
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.ramcosta.composedestinations.DestinationsNavHost
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
-import org.sopt.official.feature.NavGraphs
 import org.sopt.official.feature.update.InAppUpdateManager
 import javax.inject.Inject
+import android.Manifest
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var inAppUpdateManager: InAppUpdateManager
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK    " and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            runCatching {
+                FirebaseMessaging.getInstance().token.await()
+            }.onSuccess {
+                // TODO: Send token to server
+            }.onFailure(Timber::e)
+        }
+
         inAppUpdateManager.setOnFlexibleUpdateCompleted {
             Toast.makeText(this, "Flexible Update completed", Toast.LENGTH_SHORT).show()
         }
         lifecycle.addObserver(inAppUpdateManager)
-        setContent {
-            DestinationsNavHost(navGraph = NavGraphs.root)
-        }
+        setContentView(R.layout.activity_main)
     }
 
     @Deprecated("Deprecated in Java")
