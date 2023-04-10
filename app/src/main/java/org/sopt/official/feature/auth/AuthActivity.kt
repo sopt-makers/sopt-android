6e85d36c1a34607b301ca31c2ec60de625ed0963
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-04-10 18:29:23 +0900

    [feature/#131] :lipstick: Design auth screen

---
+++ app/src/main/java/org/sopt/official/feature/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.official.feature.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AuthActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
