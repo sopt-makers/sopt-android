c13a5b936faedb3c1ef47bd0cfdd6e71ee8770f1
choiminjung <choeminjeong@choeminjeong-ui-MacBookPro.local>
2023-04-23 02:31:58 +0900

    Mypage 틀 생성

---
+++ app/src/main/java/org/sopt/official/feature/mypage/MyPageActivity.kt
@@ -0,0 +1,113 @@
+package org.sopt.official.feature.mypage
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.sopt.official.databinding.ActivityMyPageBinding
+import org.sopt.official.domain.entity.UserState
+import org.sopt.official.domain.entity.auth.UserStatus
+import org.sopt.official.feature.main.MainActivity
+import org.sopt.official.util.serializableExtra
+import org.sopt.official.util.ui.setVisible
+import org.sopt.official.util.viewBinding
+import org.sopt.official.util.wrapper.asNullableWrapper
+import java.io.Serializable
+import java.util.*
+
+@AndroidEntryPoint
+class MyPageActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMyPageBinding::inflate)
+    private val viewModel by viewModels<MyPageViewModel>()
+    private val args by serializableExtra(MyPageActivity.StartArgs(UserState.UNAUTHENTICATED))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initStartArgs()
+        initToolbar()
+        initView()
+        initClick()
+    }
+
+    private fun initStartArgs() {
+        viewModel.userState.value = args?.userState.asNullableWrapper()
+    }
+
+    private fun initToolbar() {
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun initView() {
+        viewModel.userState
+            .flowWithLifecycle(lifecycle)
+            .onEach {
+                val isMember = !(it.get() == UserState.UNAUTHENTICATED)
+                binding.containerSoptampInfo.setVisible(isMember)
+                binding.textLogIn.setVisible(!isMember)
+                binding.iconLogIn.setVisible(!isMember)
+                binding.textLogOut.setVisible(isMember)
+                binding.iconLogOut.setVisible(isMember)
+                binding.textSignOut.setVisible(isMember)
+                binding.iconSignOut.setVisible(isMember)
+            }
+            .launchIn(lifecycleScope)
+    }
+
+    private fun initClick() {
+        binding.iconPrivateInfo.setOnClickListener {
+            // 개인정보약관 뷰 이동
+        }
+        binding.iconServiceRule.setOnClickListener {
+            // 서비스 약관 뷰 이동
+        }
+        binding.iconSendOpinion.setOnClickListener {
+            // 구글폼 이동
+        }
+        binding.iconAdjustSentence.setOnClickListener {
+            // 한 마디 편집 뷰 이동
+        }
+        binding.iconChangeNickname.setOnClickListener {
+            // 닉네임 변경 뷰 이동
+        }
+        binding.iconResetStamp.setOnClickListener {
+            // 초기화 얼럿
+        }
+        binding.iconLogOut.setOnClickListener {
+            viewModel.logOut()
+        }
+        binding.iconSignOut.setOnClickListener {
+            viewModel.signOut()
+        }
+        binding.iconLogIn.setOnClickListener {
+            setResult(ResultCode.LOG_IN.ordinal)
+            onBackPressedDispatcher.onBackPressed()
+            // Main 에서 requestActivityForResult 필요
+        }
+    }
+
+    enum class ResultCode {
+        LOG_IN,
+    }
+
+    data class StartArgs(
+        val userState: UserState
+    ) : Serializable
+
+    companion object {
+        @JvmStatic
+        fun getIntent(context: Context, args: MyPageActivity.StartArgs) = Intent(context, MyPageActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra("args", args)
+        }
+    }
+}
