242e4e4bb490fc82ab6a8884f8b08ee91e963885
choiminjung <choeminjeong@choeminjeong-ui-MacBookPro.local>
2023-04-23 04:00:18 +0900

    탈퇴하기 뷰로 추가

---
+++ app/src/main/java/org/sopt/official/feature/mypage/signOut/SignOutViewModel.kt
@@ -0,0 +1,8 @@
+package org.sopt.official.feature.mypage.signOut
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SignOutViewModel: ViewModel() {
+}
