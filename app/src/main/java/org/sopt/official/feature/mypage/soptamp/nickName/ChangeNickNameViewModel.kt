7f0f6277d96cd5d5896df0d16fb716266030fe3f
choiminjung <choeminjeong@choeminjeong-ui-MacBookPro.local>
2023-04-24 21:28:20 +0900

    API 연결

--- app/src/main/java/org/sopt/official/feature/mypage/soptamp/nickName/ChangeNickNameViewModel.kt
+++ app/src/main/java/org/sopt/official/feature/mypage/soptamp/nickName/ChangeNickNameViewModel.kt
@@ -1,17 +1,60 @@
 package org.sopt.official.feature.mypage.soptamp.nickName
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import org.sopt.official.domain.entity.UserState
+import org.sopt.official.domain.entity.main.MainViewOperationInfo
+import org.sopt.official.domain.entity.main.MainViewResult
+import org.sopt.official.domain.entity.main.MainViewUserInfo
+import org.sopt.official.stamp.domain.repository.StampRepository
+import org.sopt.official.stamp.domain.repository.UserRepository
+import org.sopt.official.stamp.feature.setting.nickname.UpdateNicknameErrorState
+import org.sopt.official.util.wrapper.asNullableWrapper
+import retrofit2.HttpException
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class ChangeNickNameViewModel @Inject constructor() : ViewModel() {
+class ChangeNickNameViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+    val backPressedSignal = MutableStateFlow(false)
+    val nickName = MutableStateFlow("")
+    val isValidNickName = MutableStateFlow(true)
 
-    fun changeNickName() {
-        // changeNickName api 콜
+    init {
+        validateNickName()
     }
 
-    fun validateNickName(string: String) {
-        // validate 체크
+    fun changeNickName() {
+        viewModelScope.launch {
+            nickName.collect {
+                userRepository.updateNickname(it)
+                    .onSuccess {
+                        backPressedSignal.value = true
+                    }.onFailure {
+                        Timber.e(it)
+                    }
+            }
+        }
+    }
+
+    fun validateNickName() {
+        viewModelScope.launch {
+            nickName.collectLatest {
+                if (it.isBlank()) isValidNickName.value = true
+                else userRepository.checkNickname(it)
+                    .onSuccess {
+                        isValidNickName.value = true
+                    }.onFailure {
+                        Timber.e(it)
+                        isValidNickName.value = false
+                    }
+            }
+        }
     }
 }
