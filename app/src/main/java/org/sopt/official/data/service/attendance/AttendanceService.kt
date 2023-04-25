1f9539d230592486507ee1454ab6cf3a85268e2a
hjh1161514 <hjh1161514@naver.com>
2023-04-25 17:33:53 +0900

    [feature/#137] 서버 end point 변경

--- app/src/main/java/org/sopt/official/data/service/attendance/AttendanceService.kt
+++ app/src/main/java/org/sopt/official/data/service/attendance/AttendanceService.kt
@@ -11,12 +11,12 @@ interface AttendanceService {
     @GET("/api/v1/app/members/attendances")
     suspend fun getAttendanceHistory(): BaseAttendanceResponse<AttendanceHistoryResponse>
 
-    @GET("/app/lectures/round/{lectureId}")
+    @GET("/api/v1/app/lectures/round/{lectureId}")
     suspend fun getAttendanceRound(
         @Path("lectureId") lectureId: Long
     ): BaseAttendanceResponse<AttendanceRoundResponse>
 
-    @GET("/app/attendances/attend")
+    @GET("/api/v1/app/attendances/attend")
     suspend fun confirmAttendanceCode(
         param: RequestAttendanceCode
     ): BaseAttendanceResponse<AttendanceCodeResponse>
