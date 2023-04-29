813d43ab1797167e951383f23e235db261ff51c3
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-04-30 01:17:36 +0900

    test junit5로 교체

--- app/src/test/java/org/sopt/official/JsonObjectTest.kt
+++ app/src/test/java/org/sopt/official/JsonObjectTest.kt
@@ -4,11 +4,13 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
-import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
 class JsonObjectTest {
     @Test
-    fun `json에 message라는 프로퍼티가 kotlinx serialization의 JsonObject로 객체화 시켰을 때에도 잘 가져와진다`() {
+    @DisplayName("json에 message라는 프로퍼티가 kotlinx serialization의 JsonObject로 객체화 시켰을 때에도 잘 가져와진다")
+    fun parsingJsonTest() {
         val json = """
             {
                 "message": "1차 출석이 이미 종료되었습니다."
