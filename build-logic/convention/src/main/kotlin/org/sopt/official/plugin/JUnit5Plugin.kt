9c97a107bdd45cbf3ec220b7277f97583e303cdd
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-04-30 01:11:59 +0900

    JUnit5 Plugin 추가

---
+++ build-logic/convention/src/main/kotlin/org/sopt/official/plugin/JUnit5Plugin.kt
@@ -0,0 +1,21 @@
+package org.sopt.official.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class JUnit5Plugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        with(plugins) {
+            apply("de.mannodermaus.android-junit5")
+        }
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+        dependencies {
+            "testImplementation"(libs.findBundle("junit5.test").get())
+            "androidTestImplementation"(libs.findLibrary("junit5").get())
+        }
+    }
+}
