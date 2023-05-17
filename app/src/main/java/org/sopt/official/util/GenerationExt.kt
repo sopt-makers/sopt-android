7a566031c79dc5aca5b93f2c470c70433589b69a
KwakEuiJin <yuijin0909@gmail.com>
2023-05-17 20:41:43 +0900

    refactor(GenerationExt): #204 java-time to kotlinx-datetime {level2}

--- app/src/main/java/org/sopt/official/util/GenerationExt.kt
+++ app/src/main/java/org/sopt/official/util/GenerationExt.kt
@@ -1,33 +1,20 @@
 package org.sopt.official.util
 
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.periodUntil
-import kotlinx.datetime.toKotlinLocalDate
+import kotlinx.datetime.plus
+import kotlinx.datetime.toLocalDate
 import kotlinx.datetime.toLocalDateTime
-import java.time.format.DateTimeFormatter
 
-private val DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-fun getGeneration(generation: Int): LocalDate =
-        java.time.LocalDate.parse(
-                when (generation) {
-                    32 -> "2023.03.01"
-                    31 -> "2022.09.01"
-                    30 -> "2022.03.01"
-                    29 -> "2021.09.01"
-                    28 -> "2021.03.01"
-                    27 -> "2020.09.01"
-                    26 -> "2020.03.01"
-                    25 -> "2019.09.01"
-                    24 -> "2019.03.01"
-                    23 -> "2018.09.01"
-                    22 -> "2018.03.01"
-                    21 -> "2017.09.01"
-                    else -> "2017.03.01"
-                },
-                DATE_FORMAT
-        ).toKotlinLocalDate()
+fun getGeneration(generation: Int): LocalDate {
+    val baseDate = "2007-03-01".toLocalDate()
+    val monthsBetweenGenerations = 6
+    val monthsToSubtract = (generation - 0) * monthsBetweenGenerations
+    return baseDate.plus(monthsToSubtract, DateTimeUnit.MONTH)
+}
 
 fun computeMothUntilNow(generation: Int): Int {
     val currentTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
