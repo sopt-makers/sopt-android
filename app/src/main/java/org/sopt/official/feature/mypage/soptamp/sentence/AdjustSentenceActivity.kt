8a65e9ae4d9faf4c90e0e154c3bab57656cb2a67
choiminjung <choeminjeong@choeminjeong-ui-MacBookPro.local>
2023-06-28 21:39:20 +0900

    adjustSentence 리팩토링

--- app/src/main/java/org/sopt/official/feature/mypage/soptamp/sentence/AdjustSentenceActivity.kt
+++ app/src/main/java/org/sopt/official/feature/mypage/soptamp/sentence/AdjustSentenceActivity.kt
@@ -5,21 +5,24 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
+import com.jakewharton.rxbinding4.view.clicks
+import com.jakewharton.rxbinding4.widget.textChanges
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import org.sopt.official.databinding.ActivityAdjustSentenceBinding
-import org.sopt.official.feature.mypage.MyPageActivity
+import org.sopt.official.util.rx.observeOnMain
+import org.sopt.official.util.rx.subscribeBy
+import org.sopt.official.util.rx.subscribeOnIo
+import org.sopt.official.util.ui.throttleUi
 import org.sopt.official.util.viewBinding
 
 @AndroidEntryPoint
 class AdjustSentenceActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityAdjustSentenceBinding::inflate)
     private val viewModel by viewModels<AdjustSentenceViewModel>()
+
+    private val createDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,22 +41,43 @@ class AdjustSentenceActivity : AppCompatActivity() {
 
     private fun initView() {
         viewModel.backPressedSignal
-            .flowWithLifecycle(lifecycle)
+            .toFlowable(BackpressureStrategy.LATEST)
             .filter { it }
-            .onEach {
-                this.onBackPressedDispatcher.onBackPressed()
-            }
-            .launchIn(lifecycleScope)
-        binding.edittext.addTextChangedListener {
-            binding.confirmButton.isEnabled = (it?.length ?: 0) > 0
-        }
+            .subscribeOnIo()
+            .observeOnMain()
+            .onBackpressureLatest()
+            .subscribeBy(createDisposable,
+                onNext = {
+                    this.onBackPressedDispatcher.onBackPressed()
+                })
+        binding.edittext.textChanges()
+            .throttleUi()
+            .map { it.isNotEmpty() }
+            .distinctUntilChanged()
+            .observeOnMain()
+            .onBackpressureLatest()
+            .subscribeBy(createDisposable,
+                onNext = {
+                    binding.confirmButton.isEnabled = it
+                })
     }
 
     private fun initClick() {
-        binding.confirmButton.setOnClickListener {
-            val text = binding.edittext.text.toString()
-            viewModel.adjustSentence(text)
-        }
+        binding.confirmButton.clicks()
+            .throttleUi()
+            .map { binding.edittext.text.toString() }
+            .distinctUntilChanged()
+            .onBackpressureLatest()
+            .subscribeBy(createDisposable,
+                onNext = {
+                    viewModel.adjustSentence(it)
+                })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        createDisposable.dispose()
     }
 
     companion object {
