20907f262fa13f4c74a4e184740dbc0a26897289
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-06-28 22:56:48 +0900

    Gradle version fix

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 group = "org.sopt.official.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
--- build-logic/convention/src/main/kotlin/org/sopt/official/plugin/AndroidKotlinPlugin.kt
+++ build-logic/convention/src/main/kotlin/org/sopt/official/plugin/AndroidKotlinPlugin.kt
@@ -28,12 +28,12 @@ class AndroidKotlinPlugin : Plugin<Project> {
 
             compileOptions {
                 isCoreLibraryDesugaringEnabled = true
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
             }
 
             (this as ExtensionAware).configure<KotlinJvmOptions> {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
 
