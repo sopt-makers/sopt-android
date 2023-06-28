6509d422445509e8aab36b0e0a2d0d2ccc492cf6
choiminjung <choeminjeong@choeminjeong-ui-MacBookPro.local>
2023-06-28 21:39:33 +0900

    ChangeNickName 리팩토링

--- app/src/main/java/org/sopt/official/feature/mypage/soptamp/nickName/ChangeNickNameViewModel.kt
+++ app/src/main/java/org/sopt/official/feature/mypage/soptamp/nickName/ChangeNickNameViewModel.kt
@@ -1,20 +1,15 @@
 package org.sopt.official.feature.mypage.soptamp.nickName
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.processors.BehaviorProcessor
+import io.reactivex.rxjava3.subjects.PublishSubject
 import kotlinx.coroutines.launch
-import org.sopt.official.domain.entity.UserState
-import org.sopt.official.domain.entity.main.MainViewOperationInfo
-import org.sopt.official.domain.entity.main.MainViewResult
-import org.sopt.official.domain.entity.main.MainViewUserInfo
-import org.sopt.official.stamp.domain.repository.StampRepository
 import org.sopt.official.stamp.domain.repository.UserRepository
-import org.sopt.official.stamp.feature.setting.nickname.UpdateNicknameErrorState
-import org.sopt.official.util.wrapper.asNullableWrapper
-import retrofit2.HttpException
+import org.sopt.official.util.rx.subscribeBy
+import org.sopt.official.util.rx.subscribeOnIo
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -22,9 +17,11 @@ import javax.inject.Inject
 class ChangeNickNameViewModel @Inject constructor(
     private val userRepository: UserRepository,
 ) : ViewModel() {
-    val backPressedSignal = MutableStateFlow(false)
-    val nickName = MutableStateFlow("")
-    val isValidNickName = MutableStateFlow(true)
+    val backPressedSignal = PublishSubject.create<Boolean>()
+    val nickName = BehaviorProcessor.createDefault("")
+    val isValidNickName = BehaviorProcessor.createDefault(true)
+
+    private val createDisposable = CompositeDisposable()
 
     init {
         validateNickName()
@@ -32,29 +29,41 @@ class ChangeNickNameViewModel @Inject constructor(
 
     fun changeNickName() {
         viewModelScope.launch {
-            nickName.collect {
-                userRepository.updateNickname(it)
-                    .onSuccess {
-                        backPressedSignal.value = true
-                    }.onFailure {
-                        Timber.e(it)
-                    }
-            }
+            nickName.first("")
+                .subscribeOnIo()
+                .subscribeBy(createDisposable,
+                    onSuccess = {
+                        backPressedSignal.onNext(true)
+                    })
         }
     }
 
     fun validateNickName() {
+        nickName.first("")
+            .subscribeOnIo()
+            .subscribeBy(createDisposable,
+                onSuccess = {
+                    if (it.isBlank()) isValidNickName.onNext(true)
+                    else checkNickName(it)
+                })
+
+    }
+
+    private fun checkNickName(nickName: String) {
         viewModelScope.launch {
-            nickName.collectLatest {
-                if (it.isBlank()) isValidNickName.value = true
-                else userRepository.checkNickname(it)
-                    .onSuccess {
-                        isValidNickName.value = true
-                    }.onFailure {
-                        Timber.e(it)
-                        isValidNickName.value = false
-                    }
-            }
+            userRepository.checkNickname(nickName)
+                .onSuccess {
+                    isValidNickName.onNext(true)
+                }.onFailure {
+                    Timber.e(it)
+                    isValidNickName.onNext(false)
+                }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        createDisposable.dispose()
     }
 }
