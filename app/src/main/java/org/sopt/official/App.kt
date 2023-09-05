c1700881c131c5c2b2e1844c510ce4485ed32828
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-09-05 20:20:55 +0900

    [feature/login-issue] FCM 토큰 기기에 저장하는 로직 추가

--- app/src/main/java/org/sopt/official/App.kt
+++ app/src/main/java/org/sopt/official/App.kt
@@ -2,16 +2,37 @@ package org.sopt.official
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.airbnb.mvrx.mocking.MockableMavericks
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import org.sopt.official.data.persistence.SoptDataStore
+import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
+    @Inject
+    lateinit var dataStore: SoptDataStore
     override fun onCreate() {
         super.onCreate()
         initFlipper()
         initMavericks()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        ProcessLifecycleOwner.get().lifecycle.coroutineScope.launch {
+            ProcessLifecycleOwner.get().lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                runCatching {
+                    FirebaseMessaging.getInstance().token.await()
+                }.onSuccess {
+                    dataStore.pushToken = it
+                }.onFailure(Timber::e)
+            }
+        }
     }
 
     private fun initMavericks() {
--- app/src/main/java/org/sopt/official/config/messaging/SoptFirebaseMessagingService.kt
+++ app/src/main/java/org/sopt/official/config/messaging/SoptFirebaseMessagingService.kt
@@ -2,18 +2,17 @@ package org.sopt.official.config.messaging
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
+import org.sopt.official.data.persistence.SoptDataStore
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SoptFirebaseMessagingService : FirebaseMessagingService() {
-    /**
-     * Called if the FCM registration token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the
-     * FCM registration token is initially generated so this is where you would retrieve the token.
-     */
-    override fun onNewToken(token: String) {
-        Timber.d("SOPT", "Refreshed token: $token")
 
+    @Inject
+    lateinit var dataStore: SoptDataStore
+
+    override fun onNewToken(token: String) {
+        dataStore.pushToken = token
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // FCM registration token to your app server.
