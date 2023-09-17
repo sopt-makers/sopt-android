7cdc48b72aa228598944cf2013cee22b430d35fb
kimdahyee <diana6777@naver.com>
2023-09-18 01:32:06 +0900

    [김다혜] 필요없는 주석 제거

--- app/src/main/java/org/sopt/official/feature/main/MainViewModel.kt
+++ app/src/main/java/org/sopt/official/feature/main/MainViewModel.kt
@@ -47,9 +47,6 @@ class MainViewModel @Inject constructor(
             val startDate = calculateGenerationStartDate(generation)
             val currentDate = Instant.systemNow().toDefaultLocalDate()
             val period = calculateDurationOfGeneration(startDate, currentDate)
-            /**
-             * todo kdh 서버 연결 예정
-             */
             when {
                 userName.isNotEmpty() -> MainTitle(R.string.main_title_member, userName, period.toString())
                 state == UserState.INACTIVE -> MainTitle(R.string.main_title_inactive_member, null, null)
