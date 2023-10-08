293b33c722409c950e59a6c57f26a16d00fe7953
yxnsx <yxnsx.dev@gmail.com>
2023-10-08 16:59:41 +0900

    [Chore/#341] NotificationHistoryItemClickListener 파일 내 불필요한 import 삭제

--- app/src/main/java/org/sopt/official/feature/notification/NotificationHistoryItemClickListener.kt
+++ app/src/main/java/org/sopt/official/feature/notification/NotificationHistoryItemClickListener.kt
@@ -1,7 +1,5 @@
 package org.sopt.official.feature.notification
 
-import org.sopt.official.data.model.notification.response.NotificationHistoryItemResponse
-
 interface NotificationHistoryItemClickListener {
     fun onClickNotificationHistoryItem(position: Int)
 }
