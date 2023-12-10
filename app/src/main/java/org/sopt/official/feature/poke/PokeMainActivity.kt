b9d89373487dc31dc849acf97499f792816fe3af
kimdahyee._ <63586451+kimdahyee@users.noreply.github.com>
2023-12-10 13:40:18 +0900

    [Feature/#488] 앱 신규피처 - 콕찌르기 메인 뷰 swipeRefreshLayout 추가

--- app/src/main/java/org/sopt/official/feature/poke/PokeMainActivity.kt
+++ app/src/main/java/org/sopt/official/feature/poke/PokeMainActivity.kt
@@ -2,11 +2,20 @@ package org.sopt.official.feature.poke
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.sopt.official.R
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.official.common.util.viewBinding
+import org.sopt.official.databinding.ActivityPokeMainBinding
 
+@AndroidEntryPoint
 class PokeMainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityPokeMainBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_poke_main)
+        setContentView(binding.root)
+
+        binding.scrollviewPokeMain.viewTreeObserver.addOnScrollChangedListener {
+            binding.refreshLayoutPokeMain.isEnabled = binding.scrollviewPokeMain.scrollY == 0
+        }
     }
 }
