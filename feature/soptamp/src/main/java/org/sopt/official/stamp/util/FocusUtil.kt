6b90fbc967635cc33b65f91b93015b4100894473
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-02-15 23:47:48 +0900

    Make function inline

--- feature/soptamp/src/main/java/org/sopt/official/stamp/util/FocusUtil.kt
+++ feature/soptamp/src/main/java/org/sopt/official/stamp/util/FocusUtil.kt
@@ -29,9 +29,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.pointer.pointerInput
 
-fun Modifier.addFocusCleaner(
+inline fun Modifier.addFocusCleaner(
     focusManager: FocusManager,
-    doOnClear: () -> Unit = {}
+    crossinline doOnClear: () -> Unit = {}
 ): Modifier {
     return this.pointerInput(Unit) {
         detectTapGestures(
