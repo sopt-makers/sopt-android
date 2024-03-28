43e09ba056499db2c03ed6e7b8ddb0d88493072a
Dongmin <pdm001125@naver.com>
2024-03-28 13:22:49 +0900

    Chore: 패키지 정리

--- feature/soptamp/src/main/java/org/sopt/official/stamp/feature/ranking/RankColor.kt
+++ feature/soptamp/src/main/java/org/sopt/official/stamp/feature/ranking/RankColor.kt
@@ -26,6 +26,7 @@ package org.sopt.official.stamp.feature.ranking
 
 import androidx.compose.runtime.Composable
 import org.sopt.official.stamp.designsystem.style.Gray300
+import org.sopt.official.stamp.designsystem.style.Gray400
 import org.sopt.official.stamp.designsystem.style.SoptTheme
 
 @Composable
