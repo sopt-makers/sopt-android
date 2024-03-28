865a04e9251b9b8469f882b61fcaf6daaa61c92f
Dongmin <pdm001125@naver.com>
2024-03-28 19:21:02 +0900

    feat: RankingBar modifier 인자 넣기

--- feature/soptamp/src/main/java/org/sopt/official/stamp/feature/ranking/RankingBar.kt
+++ feature/soptamp/src/main/java/org/sopt/official/stamp/feature/ranking/RankingBar.kt
@@ -2,19 +2,16 @@ package org.sopt.official.stamp.feature.ranking
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun RankingBar(width: Dp, height: Dp, rank: Int, content: @Composable () -> Unit = {}) {
+fun RankingBar(modifier: Modifier = Modifier, rank: Int, content: @Composable () -> Unit = {}) {
     Box(
-        modifier = Modifier
-            .size(width, height)
+        modifier = modifier
             .background(
                 color = getRankBackgroundColor(rank),
                 shape = RoundedCornerShape(8.dp)
