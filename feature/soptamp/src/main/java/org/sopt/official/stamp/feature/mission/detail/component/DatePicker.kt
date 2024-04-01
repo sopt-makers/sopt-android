81164d2df841ac295bfb2f878be472a9fefa535d
Dongmin <pdm001125@naver.com>
2024-04-01 17:58:33 +0900

    feat #644: derivedStateOf 공부 후 적용

--- feature/soptamp/src/main/java/org/sopt/official/stamp/feature/mission/detail/component/DatePicker.kt
+++ feature/soptamp/src/main/java/org/sopt/official/stamp/feature/mission/detail/component/DatePicker.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
