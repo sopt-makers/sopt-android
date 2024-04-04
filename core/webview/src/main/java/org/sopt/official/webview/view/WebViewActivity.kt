d5ed04679e78f34288a7eb577b683a05125b4e69
giovannijunseokim <kingjunseo@gmail.com>
2024-04-04 10:16:16 +0900

    chore: 변수 prefix 제거

--- core/webview/src/main/java/org/sopt/official/webview/view/WebViewActivity.kt
+++ core/webview/src/main/java/org/sopt/official/webview/view/WebViewActivity.kt
@@ -18,7 +18,7 @@ class WebViewActivity : AppCompatActivity() {
         binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mLinkUrl = intent.getStringExtra(INTENT_URL)
-        mLinkUrl?.let { binding.webView.loadUrl(it) }
+        val linkUrl = intent.getStringExtra(INTENT_URL)
+        linkUrl?.let { binding.webView.loadUrl(it) }
     }
 }
