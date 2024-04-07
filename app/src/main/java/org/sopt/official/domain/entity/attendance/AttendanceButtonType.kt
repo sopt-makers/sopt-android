ed8eadc7f271a76cecc52c8af6a37bd918a0916c
giovannijunseokim <kingjunseo@gmail.com>
2024-04-07 16:29:17 +0900

    chore: 관리할 에러 메시지 추가

--- app/src/main/java/org/sopt/official/domain/entity/attendance/AttendanceButtonType.kt
+++ app/src/main/java/org/sopt/official/domain/entity/attendance/AttendanceButtonType.kt
@@ -28,20 +28,23 @@ enum class AttendanceButtonType(
     val attendanceRound: AttendanceRound,
     val messages: List<String>,
 ) {
-    GONE_BUTTON(AttendanceRound(-1, "-"), listOf("[LectureException] : 오늘 세션이 없습니다.", "[LectureException] : 존재하지 않는 세션입니다.")),
+    GONE_BUTTON(
+        AttendanceRound(-1, "-"), listOf("[LectureException] : 오늘 세션이 없습니다.", "[LectureException] : 존재하지 않는 세션입니다.", "이미 종료된 세션입니다.")
+    ),
     BEFORE_FIRST_ATTENDANCE(
-        AttendanceRound(0, "1차 출석 전"),
-        listOf("[LectureException] : 출석 시작 전입니다.", "[LectureException] : 1차 출석 시작 전입니다.")
+        AttendanceRound(0, "1차 출석 전"), listOf("[LectureException] : 출석 시작 전입니다.", "[LectureException] : 1차 출석 시작 전입니다.")
     ),
     BEFORE_SECOND_ATTENDANCE(
-        AttendanceRound(0, "2차 출석 전"),
-        listOf("[LectureException] : 2차 출석 시작 전입니다.", "[LectureException] : 1차 출석이 이미 종료되었습니다.")
+        AttendanceRound(0, "2차 출석 전"), listOf("[LectureException] : 2차 출석 시작 전입니다.", "[LectureException] : 1차 출석이 이미 종료되었습니다.")
     ),
-    AFTER_SECOND_ATTENDANCE(AttendanceRound(0, "2차 출석 종료"), listOf("[LectureException] : 2차 출석이 이미 종료되었습니다.")),
-    ERROR(AttendanceRound(-2, ""), listOf());
+    AFTER_SECOND_ATTENDANCE(AttendanceRound(0, "2차 출석 종료"), listOf("[LectureException] : 2차 출석이 이미 종료되었습니다.")), ERROR(
+        AttendanceRound(
+            -2, ""
+        ), listOf()
+    );
 
     companion object {
-        fun of(message: String) = entries.find { entry -> entry.messages.any { it.contains(message) } }?.attendanceRound
-            ?: ERROR.attendanceRound
+        fun of(message: String) =
+            entries.find { entry -> entry.messages.any { it.contains(message) } }?.attendanceRound ?: ERROR.attendanceRound
     }
 }
