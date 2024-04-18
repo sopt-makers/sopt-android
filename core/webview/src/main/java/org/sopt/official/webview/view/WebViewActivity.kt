8b0161484d70f1a959872d5ed4f97bcb7dea6f55
giovannijunseokim <kingjunseo@gmail.com>
2024-04-18 17:43:58 +0900

    feat: handle OnBackPressed

--- core/webview/src/main/java/org/sopt/official/webview/view/WebViewActivity.kt
+++ core/webview/src/main/java/org/sopt/official/webview/view/WebViewActivity.kt
@@ -1,6 +1,7 @@
 package org.sopt.official.webview.view
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.official.common.util.viewBinding
@@ -18,7 +19,22 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        handleLinkUrl()
+        handleOnBackPressed()
+    }
+
+    private fun handleLinkUrl() {
         val linkUrl = intent.getStringExtra(INTENT_URL)
         linkUrl?.let { binding.webView.loadUrl(it) }
+    }
+
+    private fun handleOnBackPressed() {
+        onBackPressedDispatcher.addCallback(owner = this) {
+            if (binding.webView.canGoBack()) {
+                binding.webView.goBack()
+            } else {
+                if (!isFinishing) finish()
+            }
+        }
     }
 }
