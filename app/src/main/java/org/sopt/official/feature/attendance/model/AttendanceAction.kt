6061526de89ff082c34d7c7fa62a5350dad72ac9
giovannijunseokim <kingjunseo@gmail.com>
2024-04-19 18:29:36 +0900

    chore: data class -> class로 변경

--- app/src/main/java/org/sopt/official/feature/attendance/model/AttendanceAction.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/model/AttendanceAction.kt
@@ -2,6 +2,6 @@ package org.sopt.official.feature.attendance.model
 
 import org.sopt.official.feature.attendance.NewAttendanceViewModel
 
-data class AttendanceAction(private val viewModel: NewAttendanceViewModel) {
+class AttendanceAction(private val viewModel: NewAttendanceViewModel) {
     val onFakeClick: () -> Unit = viewModel::updateUiState
 }
