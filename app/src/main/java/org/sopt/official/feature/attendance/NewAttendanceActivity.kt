049be6ce45309b1d93fc1b96488d3abafebeece3
giovannijunseokim <kingjunseo@gmail.com>
2024-04-19 17:47:41 +0900

    feat: use SoptTheme in designsystem

--- app/src/main/java/org/sopt/official/feature/attendance/NewAttendanceActivity.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/NewAttendanceActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.official.designsystem.SoptTheme
 import org.sopt.official.feature.attendance.compose.AttendanceRoute
-import org.sopt.official.feature.ui.theme.SOPTTheme
 
 @AndroidEntryPoint
 class NewAttendanceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SOPTTheme(darkTheme = false) {
+            SoptTheme(darkTheme = false) {
                 AttendanceRoute()
             }
         }
