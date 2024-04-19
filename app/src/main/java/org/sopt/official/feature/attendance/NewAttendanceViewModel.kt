e19a2264a377f01594005d291f84502ab7ded887
giovannijunseokim <kingjunseo@gmail.com>
2024-04-19 17:31:49 +0900

    feat: implement NewAttendanceViewModel

---
+++ app/src/main/java/org/sopt/official/feature/attendance/NewAttendanceViewModel.kt
@@ -0,0 +1,32 @@
+package org.sopt.official.feature.attendance
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.sopt.official.domain.repository.attendance.AttendanceRepository
+import org.sopt.official.feature.attendance.model.AttendanceUiState
+import javax.inject.Inject
+
+@HiltViewModel
+class NewAttendanceViewModel @Inject constructor(
+    private val attendanceRepository: AttendanceRepository
+) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<AttendanceUiState> = MutableStateFlow(AttendanceUiState.Loading)
+    val uiState: StateFlow<AttendanceUiState> = _uiState
+
+    private var fakeTitle: String = ""
+
+    fun updateUiState() {
+        viewModelScope.launch {
+            _uiState.emit(AttendanceUiState.Success(fakeTitle))
+        }
+    }
+
+    fun onClickFake() {
+
+    }
+}
