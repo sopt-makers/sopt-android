33edc7cd6c1c6824a468eb6f99bf631bb5e8dcf9
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-04-22 14:00:25 +0900

    Attendance screen compose setting (#676)
    * feat: implement NewAttendanceActivity
    
    * feat: implement NewAttendanceViewModel
    
    * chore: add compose-lifecycle dependency
    
    * feat: define AttendanceAction
    
    * feat: implement screens
    
    * feat: use SoptTheme in designsystem
    
    * chore: data class -> class로 변경
    
    * chore: 람다 프로퍼티 이름 명시
    
    * chore: SoptTheme darkTheme 기본값 사용
    
    * chore: 필요없는 함수 제거
    
    * chore: 구현 안 된 함수에 TODO 삽입
    
    * chore: 동작하지 않는 Preview 제거
    
    * chore: AttendanceAction 내 뷰모델 참조 제거
    
    * chore: code format 변경

---
+++ app/src/main/java/org/sopt/official/feature/attendance/NewAttendanceActivity.kt
@@ -0,0 +1,20 @@
+package org.sopt.official.feature.attendance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.feature.attendance.compose.AttendanceRoute
+
+@AndroidEntryPoint
+class NewAttendanceActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SoptTheme {
+                AttendanceRoute()
+            }
+        }
+    }
+}
