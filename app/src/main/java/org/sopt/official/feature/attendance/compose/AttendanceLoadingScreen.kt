3e34302ca9baa5dcdf032487fe86bea22121ff71
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-04-22 14:00:25 +0900

    Attendance screen compose setting (#676)
    * feat: implement NewAttendanceActivity
    
    * feat: implement NewAttendanceViewModel
    
    * chore: add compose-lifecycle dependency
    
    * feat: define AttendanceAction
    
    * feat: implement screens
    
    * feat: use SoptTheme in designsystem
    
    * chore: data class -> class로 변경
    
    * chore: 람다 프로퍼티 이름 명시
    
    * chore: SoptTheme darkTheme 기본값 사용
    
    * chore: 필요없는 함수 제거
    
    * chore: 구현 안 된 함수에 TODO 삽입
    
    * chore: 동작하지 않는 Preview 제거
    
    * chore: AttendanceAction 내 뷰모델 참조 제거
    
    * chore: code format 변경

---
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceLoadingScreen.kt
@@ -0,0 +1,20 @@
+package org.sopt.official.feature.attendance.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AttendanceLoadingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Loading")
+    }
+}
---
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceRoute.kt
@@ -0,0 +1,58 @@
+package org.sopt.official.feature.attendance.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import org.sopt.official.feature.attendance.NewAttendanceViewModel
+import org.sopt.official.feature.attendance.model.AttendanceAction
+import org.sopt.official.feature.attendance.model.AttendanceUiState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AttendanceRoute() {
+    val viewModel: NewAttendanceViewModel = viewModel()
+
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+    val action = viewModel.rememberAttendanceActions()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "출석") })
+        }
+    ) { innerPaddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPaddingValues)
+        ) {
+            when (state) {
+                AttendanceUiState.Loading -> {
+                    AttendanceLoadingScreen()
+                }
+
+                is AttendanceUiState.Failure -> {}
+                AttendanceUiState.NetworkError -> {}
+                is AttendanceUiState.Success -> {
+                    AttendanceScreen(state = state as AttendanceUiState.Success, action = action)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NewAttendanceViewModel.rememberAttendanceActions(): AttendanceAction = remember(this) {
+    AttendanceAction(
+        onFakeClick = this::updateUiState
+    )
+}
