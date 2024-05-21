2063d1c550ec6fa84b8b2e643c858c1edc3547cb
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-21 20:51:54 +0900

    Apply Compose Compiler Plugin in Project Pluign

--- build-logic/convention/src/main/kotlin/org/sopt/official/plugin/AndroidComposePlugin.kt
+++ build-logic/convention/src/main/kotlin/org/sopt/official/plugin/AndroidComposePlugin.kt
@@ -6,22 +6,30 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 class AndroidComposePlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
+        with(plugins) {
+            apply("org.jetbrains.kotlin.plugin.compose")
+        }
         val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
         extensions.getByType<BaseExtension>().apply {
             buildFeatures.apply {
                 compose = true
             }
-            composeOptions {
-                kotlinCompilerExtensionVersion = libs.findVersion("compose-compiler").get().requiredVersion
-            }
+        }
+        extensions.getByType<ComposeCompilerGradlePluginExtension>().apply {
+            enableStrongSkippingMode.set(true)
+            includeSourceInformation.set(true)
+            metricsDestination.set(file("build/compose-metrics"))
+            reportsDestination.set(file("build/compose-reports"))
         }
         dependencies {
             "implementation"(platform(libs.findLibrary("compose-bom").get()))
             "implementation"(libs.findBundle("compose").get())
             "implementation"(libs.findLibrary("coil-compose").get())
+            "implementation"(libs.findLibrary("compose-compiler-extension").get())
         }
     }
 }
