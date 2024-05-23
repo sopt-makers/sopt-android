610ddf0a52bb3441e592334e69097f245918ef9a
giovannijunseokim <kingjunseo@gmail.com>
2024-05-23 10:07:48 +0900

    feat: AttendanceHistorySummaryCard

---
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceHistorySummaryCard.kt
@@ -0,0 +1,19 @@
+package org.sopt.official.feature.attendance.compose
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AttendanceHistorySummaryCard(modifier: Modifier = Modifier) {
+    Row {
+        
+    }
+}
+
+@Preview
+@Composable
+fun AttendanceHistorySummaryCardPreview() {
+    AttendanceHistorySummaryCard()
+}
