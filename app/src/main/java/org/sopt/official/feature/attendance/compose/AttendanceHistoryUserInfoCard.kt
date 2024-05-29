77a924013c92a98871339f25a813c66305853beb
giovannijunseokim <kingjunseo@gmail.com>
2024-05-30 02:26:03 +0900

    chore: add text style to AttendanceHistoryUserInfoCard

--- app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceHistoryUserInfoCard.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceHistoryUserInfoCard.kt
@@ -9,22 +9,44 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.R
+import org.sopt.official.designsystem.Orange400
 import org.sopt.official.designsystem.SoptTheme
 
 @Composable
-fun AttendanceHistoryUserInfoCard(userTitle: String, attendanceScore: Int, modifier: Modifier = Modifier) {
+fun AttendanceHistoryUserInfoCard(
+    userTitle: String,
+    attendanceScore: Int,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier) {
-        Text(text = userTitle, color = SoptTheme.colors.onSurface300)
+        Text(
+            text = userTitle,
+            color = SoptTheme.colors.onSurface300,
+            style = SoptTheme.typography.body14M
+        )
         Spacer(modifier = Modifier.height(8.dp))
-        Row {
-            Text(text = "현재 출석점수는 ", color = SoptTheme.colors.onSurface10)
-            Text(text = "${attendanceScore}점", color = SoptTheme.colors.onSurface10)
-            Text(text = " 입니다!", color = SoptTheme.colors.onSurface10)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "현재 출석점수는 ",
+                color = SoptTheme.colors.onSurface10,
+                style = SoptTheme.typography.body18M
+            )
+            Text(
+                text = "${attendanceScore}점",
+                color = Orange400,
+                style = SoptTheme.typography.title20SB
+            )
+            Text(
+                text = " 입니다!",
+                color = SoptTheme.colors.onSurface10,
+                style = SoptTheme.typography.body18M
+            )
             Spacer(modifier = Modifier.weight(1f))
             Icon(
                 modifier = Modifier.padding(end = 2.dp),
