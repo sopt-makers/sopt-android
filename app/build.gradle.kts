a814a6a452ce11260007e51469d9c330719d0739
giovannijunseokim <kingjunseo@gmail.com>
2024-05-30 00:31:18 +0900

    chore: add dependency for collectAsStateWithLifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,6 +131,7 @@ dependencies {
     implementation(projects.feature.auth)
     implementation(projects.feature.mypage)
     implementation(projects.feature.poke)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
     baselineProfile(projects.baselineprofile)
 
     implementation(libs.kotlin.coroutines.google.play)
