e9af7381fc4e896bd522350383c5d478419747f2
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-05-30 10:25:22 +0900

    Attendance top app bar (#716)
    * chore: core 모듈의 designsystem에 NoRippleClickable 추가
    
    * feat: AttendanceTopAppBar 생성
    
    * chore: String 리소스 추출
    
    * chore: TODO : fetchData 구현
    
    * chore: AttendanceTopAppBar 적용
    
    * chore: Modifier 인자 추가
    
    * chore: extract string resource
    
    * chore: change style and delete action class
    
    * chore: change icon
    
    * chore: delete NoRippleClickable
    
    * chore: use clickable

--- app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceRoute.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/AttendanceRoute.kt
@@ -3,10 +3,7 @@ package org.sopt.official.feature.attendance.compose
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -14,21 +11,23 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.sopt.official.feature.attendance.NewAttendanceViewModel
+import org.sopt.official.feature.attendance.compose.component.AttendanceTopAppBar
 import org.sopt.official.feature.attendance.model.AttendanceAction
 import org.sopt.official.feature.attendance.model.AttendanceUiState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AttendanceRoute() {
+fun AttendanceRoute(onClickBackIcon: () -> Unit) {
     val viewModel: NewAttendanceViewModel = viewModel()
-
     val state by viewModel.uiState.collectAsStateWithLifecycle()
     val action = viewModel.rememberAttendanceActions()
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "출석") })
-        }
+            AttendanceTopAppBar(
+                onClickBackIcon = onClickBackIcon,
+                onClickRefreshIcon = viewModel::fetchData,
+            )
+        },
     ) { innerPaddingValues ->
         Column(
             modifier = Modifier
