71164e93762af9c1dcde808939eff29dcb976883
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-05-31 13:44:15 +0900

    Today attendance card (#717)
    * chore: icon 리소스 추가
    
    * feat: MidtermAttendanceCard 구현
    
    * feat: FinalAttendanceCard 구현
    
    * feat: AttendanceProgressBar 구현
    
    * feat: AttendanceHistoryCard 구현
    
    * chore: string 리소스 추출
    
    * chore: rename
    
    * chore: change class name
    
    * chore: add text style
    
    * chore: add AttendanceHistoryCard radius
    
    * feat: implement TodayNoAttendanceCard
    
    * feat: implement TodayNoScheduleCard
    
    * chore: extract string resource
    
    * chore: separate model
    
    * chore: use PreviewParameter
    
    * chore: apply spotlessApply
    
    * chore: change class, objects in sealed to data class, data objects

---
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/FinalAttendanceCard.kt
@@ -0,0 +1,57 @@
+package org.sopt.official.feature.attendance.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.feature.attendance.model.FinalAttendance
+
+@Composable
+fun FinalAttendanceCard(finalAttendance: FinalAttendance, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(horizontal = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(finalAttendance.imageResId),
+            contentDescription = null,
+        )
+        Text(
+            text = finalAttendance.result,
+            color = if (finalAttendance.isFinished) SoptTheme.colors.onSurface10 else SoptTheme.colors.onSurface500,
+            style = SoptTheme.typography.label14SB
+        )
+    }
+}
+
+class FinalAttendanceCardPreviewParameterProvider(
+    override val values: Sequence<FinalAttendance> = sequenceOf(
+        FinalAttendance.NOT_YET,
+        FinalAttendance.PRESENT,
+        FinalAttendance.LATE,
+        FinalAttendance.ABSENT,
+    )
+) : PreviewParameterProvider<FinalAttendance>
+
+@Preview
+@Composable
+private fun FinalAttendanceCardPreview(
+    @PreviewParameter(FinalAttendanceCardPreviewParameterProvider::class) finalAttendance: FinalAttendance
+) {
+    SoptTheme {
+        FinalAttendanceCard(
+            finalAttendance = finalAttendance,
+            modifier = Modifier.background(color = SoptTheme.colors.onSurface800)
+        )
+    }
+}
---
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/MidtermAttendanceCard.kt
@@ -0,0 +1,55 @@
+package org.sopt.official.feature.attendance.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.feature.attendance.model.AttendanceType
+import org.sopt.official.feature.attendance.model.MidtermAttendance
+
+@Composable
+fun MidtermAttendanceCard(midtermAttendance: MidtermAttendance, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(horizontal = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(midtermAttendance.imageResId),
+            contentDescription = null,
+        )
+        Text(
+            text = midtermAttendance.description,
+            color = if (midtermAttendance.isFinished) SoptTheme.colors.onSurface10 else SoptTheme.colors.onSurface500,
+            style = SoptTheme.typography.label14SB
+        )
+    }
+}
+
+class MidtermAttendanceCardPreviewParameterProvider(
+    override val values: Sequence<MidtermAttendance> = sequenceOf(
+        MidtermAttendance.NotYet(attendanceType = AttendanceType.FIRST),
+        MidtermAttendance.Present(attendanceAt = "14:00"),
+        MidtermAttendance.Absent,
+    )
+) : PreviewParameterProvider<MidtermAttendance>
+
+@Preview(showBackground = true)
+@Composable
+private fun MidtermAttendanceCardPreview(
+    @PreviewParameter(MidtermAttendanceCardPreviewParameterProvider::class) midtermAttendance: MidtermAttendance,
+) {
+    SoptTheme {
+        MidtermAttendanceCard(
+            midtermAttendance = midtermAttendance
+        )
+    }
+}
