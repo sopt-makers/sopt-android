e5ac1bcb61a387267348f09e9d1ca9f4f476bd5e
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-05-31 13:44:15 +0900

    Today attendance card (#717)
    * chore: icon 리소스 추가
    
    * feat: MidtermAttendanceCard 구현
    
    * feat: FinalAttendanceCard 구현
    
    * feat: AttendanceProgressBar 구현
    
    * feat: AttendanceHistoryCard 구현
    
    * chore: string 리소스 추출
    
    * chore: rename
    
    * chore: change class name
    
    * chore: add text style
    
    * chore: add AttendanceHistoryCard radius
    
    * feat: implement TodayNoAttendanceCard
    
    * feat: implement TodayNoScheduleCard
    
    * chore: extract string resource
    
    * chore: separate model
    
    * chore: use PreviewParameter
    
    * chore: apply spotlessApply
    
    * chore: change class, objects in sealed to data class, data objects

---
+++ app/src/main/java/org/sopt/official/feature/attendance/model/state/TodayNoAttendanceCardState.kt
@@ -0,0 +1,7 @@
+package org.sopt.official.feature.attendance.model.state
+
+class TodayNoAttendanceCardState(
+    val eventDate: String,
+    val eventLocation: String,
+    val eventName: String,
+)
