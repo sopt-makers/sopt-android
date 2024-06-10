8a52a3743b0869ceeb40a1c66f87764b690f0f18
giovannijunseokim <kingjunseo@gmail.com>
2024-06-10 15:12:30 +0900

    feat: implement AttendanceHistoryCard

--- app/src/main/java/org/sopt/official/feature/attendance/compose/component/AttendanceHistoryCard.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/compose/component/AttendanceHistoryCard.kt
@@ -1,5 +1,6 @@
 package org.sopt.official.feature.attendance.compose.component
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,25 +9,32 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.feature.attendance.model.AttendanceHistory
 import org.sopt.official.feature.attendance.model.AttendanceResultType
-import org.sopt.official.feature.attendance.model.state.AttendanceHistoryCardState
 
 @Composable
-fun AttendanceHistoryCard(state: AttendanceHistoryCardState, modifier: Modifier = Modifier) {
+fun AttendanceHistoryCard(
+    userTitle: String,
+    attendanceScore: Int,
+    totalAttendanceResult: Map<AttendanceResultType, Int>,
+    attendanceHistoryList: List<AttendanceHistory>,
+    scrollState: ScrollState, modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier
             .background(color = SoptTheme.colors.onSurface800)
             .padding(all = 32.dp)
     ) {
         AttendanceHistoryUserInfoCard(
-            userTitle = state.userTitle,
-            attendanceScore = state.attendanceScore
+            userTitle = userTitle,
+            attendanceScore = attendanceScore
         )
         Spacer(modifier = Modifier.height(24.dp))
         Row(
@@ -36,7 +44,7 @@ fun AttendanceHistoryCard(state: AttendanceHistoryCardState, modifier: Modifier 
                 .background(color = SoptTheme.colors.onSurface700, shape = RoundedCornerShape(8.dp))
                 .padding(horizontal = 24.dp, vertical = 16.dp),
         ) {
-            state.totalAttendanceResult.map { attendanceResult ->
+            totalAttendanceResult.forEach { attendanceResult ->
                 AttendanceCountCard(
                     resultType = attendanceResult.key.type,
                     count = attendanceResult.value
@@ -44,6 +52,11 @@ fun AttendanceHistoryCard(state: AttendanceHistoryCardState, modifier: Modifier 
             }
         }
         Spacer(modifier = Modifier.height(24.dp))
+        AttendanceHistoryListCard(
+            attendanceHistoryList = attendanceHistoryList,
+            scrollState = scrollState,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
@@ -52,16 +65,26 @@ fun AttendanceHistoryCard(state: AttendanceHistoryCardState, modifier: Modifier 
 private fun AttendanceHistoryCardPreview() {
     SoptTheme {
         AttendanceHistoryCard(
-            state = AttendanceHistoryCardState(
-                userTitle = "32기 디자인파트 김솝트",
-                attendanceScore = 1,
-                totalAttendanceResult = mapOf(
-                    Pair(AttendanceResultType.ALL, 16),
-                    Pair(AttendanceResultType.PRESENT, 5),
-                    Pair(AttendanceResultType.LATE, 0),
-                    Pair(AttendanceResultType.ABSENT, 11)
-                )
-            )
+            userTitle = "32기 디자인파트 김솝트",
+            attendanceScore = 1,
+            totalAttendanceResult = mapOf(
+                Pair(AttendanceResultType.ALL, 16),
+                Pair(AttendanceResultType.PRESENT, 5),
+                Pair(AttendanceResultType.LATE, 0),
+                Pair(AttendanceResultType.ABSENT, 11)
+            ),
+            attendanceHistoryList = listOf(
+                AttendanceHistory(
+                    status = "출석", eventName = "1차 세미나", date = "00월 00일"
+                ),
+                AttendanceHistory(
+                    status = "출석", eventName = "1차 세미나", date = "00월 00일"
+                ),
+                AttendanceHistory(
+                    status = "출석", eventName = "1차 세미나", date = "00월 00일"
+                ),
+            ),
+            scrollState = rememberScrollState()
         )
     }
 }
--- app/src/main/java/org/sopt/official/feature/attendance/model/state/AttendanceHistoryCardState.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.official.feature.attendance.model.state
-
-import org.sopt.official.feature.attendance.model.AttendanceResultType
-
-class AttendanceHistoryCardState(
-    val userTitle: String,
-    val attendanceScore: Int,
-    val totalAttendanceResult: Map<AttendanceResultType, Int>
-)
