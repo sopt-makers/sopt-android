07713a9290385893cc1704ac0dc540fbc777b8f7
LEE YOU BIN <youbeene3409@naver.com>
2024-09-16 16:39:13 +0900

    feat #841: mypage UI 구현

---
+++ feature/mypage/src/main/java/org/sopt/official/feature/mypage/component/MyPageItem.kt
@@ -0,0 +1,48 @@
+package org.sopt.official.feature.mypage.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import org.sopt.official.designsystem.Gray80
+import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.designsystem.White
+import org.sopt.official.feature.mypage.R
+
+@Composable
+fun MyPageItem(
+    text: String,
+    modifier: Modifier = Modifier,
+    onButtonClick: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+                onButtonClick()
+            },
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.padding(start = 16.dp),
+            style = SoptTheme.typography.body16M,
+            color = White
+        )
+        Icon(
+            painter = painterResource(R.drawable.btn_arrow_right),
+            contentDescription = "arrow button",
+            tint = Gray80,
+            modifier = Modifier.padding(end = 8.dp)
+        )
+    }
+}
--- feature/mypage/src/main/java/org/sopt/official/feature/mypage/mypage/MyPageActivity.kt
+++ feature/mypage/src/main/java/org/sopt/official/feature/mypage/mypage/MyPageActivity.kt
@@ -32,6 +32,29 @@ import android.provider.Settings
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.jakewharton.processphoenix.ProcessPhoenix
@@ -44,8 +67,12 @@ import org.sopt.official.common.util.serializableExtra
 import org.sopt.official.common.util.setOnSingleClickListener
 import org.sopt.official.common.util.ui.setVisible
 import org.sopt.official.common.util.viewBinding
+import org.sopt.official.designsystem.Black80
+import org.sopt.official.designsystem.Gray80
+import org.sopt.official.designsystem.SoptTheme
 import org.sopt.official.feature.mypage.AlertDialogPositiveNegative
 import org.sopt.official.feature.mypage.R
+import org.sopt.official.feature.mypage.component.MyPageItem
 import org.sopt.official.feature.mypage.databinding.ActivityMyPageBinding
 import org.sopt.official.feature.mypage.model.MyPageUiState
 import org.sopt.official.feature.mypage.signOut.SignOutActivity
@@ -67,21 +94,87 @@ class MyPageActivity : AppCompatActivity() {
     @Inject
     lateinit var navigatorProvider: NavigatorProvider
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
+            SoptTheme {
+                val isAuthenticated by viewModel.userActiveState.collectAsStateWithLifecycle(initialValue = false)
+                val scrollState = rememberScrollState()
 
-
+                Scaffold(modifier = Modifier
+                    .background(SoptTheme.colors.background)
+                    .fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(id = R.string.toolbar_mypage),
+                                    style = SoptTheme.typography.body16M
+                                )
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { onBackPressedDispatcher.onBackPressed() }) {
+                                    Icon(
+                                        painterResource(R.drawable.btn_arrow_left),
+                                        contentDescription = null,
+                                        tint = SoptTheme.colors.onBackground
+                                    )
+                                }
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = SoptTheme.colors.background,
+                                titleContentColor = SoptTheme.colors.onBackground,
+                                actionIconContentColor = SoptTheme.colors.primary
+                            )
+                        )
+                    }) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .background(SoptTheme.colors.background)
+                            .verticalScroll(scrollState)
+                    ) {
+                        Spacer(modifier = Modifier.height(20.dp))
+                        ServiceInfo(
+                            onPrivateClick = { },
+                            onServiceClick = { },
+                            onOpinionClick = { }
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        if (isAuthenticated) {
+                            NotificationSetting(
+                                onNotificationClick = { }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            SoptampInfo(
+                                onSentenceClick = { },
+                                onResetStampClick = { }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Etc(
+                                onLogoutClick = { },
+                                onSignOutClick = { }
+                            )
+                        } else {
+                            EtcLogin(
+                                onLoginClick = {}
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+                    }
+                }
+            }
         }
 
 
 //        setContentView(binding.root)
 //
-//        initStartArgs()
-//        initToolbar()
+        initStartArgs()
 //        initView()
 //        initClick()
-//        initRestart()
+        initRestart()
 //
 //        initNotificationSettingClickListener()
     }
@@ -92,29 +185,20 @@ class MyPageActivity : AppCompatActivity() {
         }
     }
 
-    private fun initToolbar() {
-        with(binding.includeAppBarBackArrow) {
-            textViewTitle.text = getString(R.string.toolbar_mypage)
-            toolbar.setOnSingleClickListener {
-                onBackPressedDispatcher.onBackPressed()
-            }
-        }
-    }
-
-    private fun initView() {
-        viewModel.userActiveState
-            .flowWithLifecycle(lifecycle)
-            .onEach { isAuthenticated ->
-                binding.containerNotificationSetting.setVisible(isAuthenticated)
-                binding.containerSoptampInfo.setVisible(isAuthenticated)
-                binding.textLogIn.setVisible(!isAuthenticated)
-                binding.iconLogIn.setVisible(!isAuthenticated)
-                binding.textLogOut.setVisible(isAuthenticated)
-                binding.iconLogOut.setVisible(isAuthenticated)
-                binding.textSignOut.setVisible(isAuthenticated)
-                binding.iconSignOut.setVisible(isAuthenticated)
-            }.launchIn(lifecycleScope)
-    }
+//    private fun initView() {
+//        viewModel.userActiveState
+//            .flowWithLifecycle(lifecycle)
+//            .onEach { isAuthenticated ->
+//                binding.containerNotificationSetting.setVisible(isAuthenticated)
+//                binding.containerSoptampInfo.setVisible(isAuthenticated)
+//                binding.textLogIn.setVisible(!isAuthenticated)
+//                binding.iconLogIn.setVisible(!isAuthenticated)
+//                binding.textLogOut.setVisible(isAuthenticated)
+//                binding.iconLogOut.setVisible(isAuthenticated)
+//                binding.textSignOut.setVisible(isAuthenticated)
+//                binding.iconSignOut.setVisible(isAuthenticated)
+//            }.launchIn(lifecycleScope)
+//    }
 
     private fun initClick() {
         binding.layoutPrivaceInfo.setOnSingleClickListener {
@@ -185,5 +269,179 @@ class MyPageActivity : AppCompatActivity() {
         fun getIntent(context: Context, args: StartArgs) = Intent(context, MyPageActivity::class.java).apply {
             putExtra("args", args)
         }
+    }
+}
+
+@Composable
+fun ServiceInfo(
+    modifier: Modifier = Modifier,
+    onPrivateClick: () -> Unit,
+    onServiceClick: () -> Unit,
+    onOpinionClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 20.dp)
+            .background(
+                color = Black80,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo : style
+        Text(
+            text = stringResource(id = R.string.mypage_service_info_title),
+            style = SoptTheme.typography.label12SB,
+            color = Gray80,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(23.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_private_info),
+            onButtonClick = onPrivateClick
+        )
+        Spacer(modifier = Modifier.height(22.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_service_rule),
+            onButtonClick = onServiceClick
+        )
+        Spacer(modifier = Modifier.height(22.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_send_opinion),
+            onButtonClick = onOpinionClick
+        )
+        Spacer(modifier = Modifier.height(27.dp))
+    }
+}
+
+@Composable
+fun NotificationSetting(
+    modifier: Modifier = Modifier,
+    onNotificationClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 20.dp)
+            .background(
+                color = Black80,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo : style
+        Text(
+            text = stringResource(id = R.string.mypage_notification_setting),
+            style = SoptTheme.typography.label12SB,
+            color = Gray80,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(23.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_notification),
+            onButtonClick = onNotificationClick
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun SoptampInfo(
+    modifier: Modifier = Modifier,
+    onSentenceClick: () -> Unit,
+    onResetStampClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 20.dp)
+            .background(
+                color = Black80,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo : style
+        Text(
+            text = stringResource(id = R.string.mypage_soptamp_setting_title),
+            style = SoptTheme.typography.label12SB,
+            color = Gray80,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(23.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_adjust_sentence),
+            onButtonClick = onSentenceClick
+        )
+        Spacer(modifier = Modifier.height(22.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_reset_stamp),
+            onButtonClick = onResetStampClick
+        )
+        Spacer(modifier = Modifier.height(27.dp))
+    }
+}
+
+@Composable
+fun Etc(
+    modifier: Modifier = Modifier,
+    onLogoutClick: () -> Unit,
+    onSignOutClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 20.dp)
+            .background(
+                color = Black80,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo : style
+        Text(
+            text = stringResource(id = R.string.mypage_etc_title),
+            style = SoptTheme.typography.label12SB,
+            color = Gray80,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(23.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_log_out),
+            onButtonClick = onLogoutClick
+        )
+        Spacer(modifier = Modifier.height(22.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_sign_out),
+            onButtonClick = onSignOutClick
+        )
+        Spacer(modifier = Modifier.height(27.dp))
+    }
+}
+
+@Composable
+fun EtcLogin(
+    modifier: Modifier = Modifier,
+    onLoginClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 20.dp)
+            .background(
+                color = Black80,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo : style
+        Text(
+            text = stringResource(id = R.string.mypage_etc_title),
+            style = SoptTheme.typography.label12SB,
+            color = Gray80,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Spacer(modifier = Modifier.height(23.dp))
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_log_in),
+            onButtonClick = onLoginClick
+        )
+        Spacer(modifier = Modifier.height(27.dp))
     }
 }
