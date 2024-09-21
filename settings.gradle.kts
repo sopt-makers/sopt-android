746bed252de12c409feb26bfbf2d47764f6c5b2f
Dongmin <pdm001125@naver.com>
2024-09-21 19:08:22 +0900

    feature #866: fix space 2->4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,43 +1,43 @@
 pluginManagement {
-  includeBuild("build-logic")
-  repositories {
-    gradlePluginPortal()
-    google()
-    mavenCentral()
-  }
+    includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
 }
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-    gradlePluginPortal()
-    maven { setUrl("https://jitpack.io") }
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { setUrl("https://jitpack.io") }
+    }
 }
 rootProject.name = "SOPT"
 include(
-  ":app",
-  ":baselineprofile",
-  ":core:network",
-  ":core:analytics",
-  ":core:auth",
-  ":core:authimpl",
-  ":core:common",
-  ":core:designsystem",
-  ":core:webview",
-  ":data:mypage",
-  ":data:notification",
-  ":data:soptamp",
-  ":data:poke",
-  ":domain:mypage",
-  ":domain:notification",
-  ":domain:soptamp",
-  ":domain:poke",
-  ":feature:auth",
-  ":feature:mypage",
-  ":feature:notification",
-  ":feature:soptamp",
-  ":feature:poke",
+    ":app",
+    ":baselineprofile",
+    ":core:network",
+    ":core:analytics",
+    ":core:auth",
+    ":core:authimpl",
+    ":core:common",
+    ":core:designsystem",
+    ":core:webview",
+    ":data:mypage",
+    ":data:notification",
+    ":data:soptamp",
+    ":data:poke",
+    ":domain:mypage",
+    ":domain:notification",
+    ":domain:soptamp",
+    ":domain:poke",
+    ":feature:auth",
+    ":feature:mypage",
+    ":feature:notification",
+    ":feature:soptamp",
+    ":feature:poke",
     ":feature:fortune"
 )
