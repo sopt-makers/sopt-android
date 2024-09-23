5c38463a6085f77f6e42f27e2614f920682a81e9
Dongmin <pdm001125@naver.com>
2024-09-23 14:21:46 +0900

    feature #866: feat FortuneTopBar preview

--- feature/fortune/src/main/java/org/sopt/official/feature/fortune/component/FortuneTopBar.kt
+++ feature/fortune/src/main/java/org/sopt/official/feature/fortune/component/FortuneTopBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.designsystem.SoptTheme
 
@@ -27,5 +28,15 @@ fun FortuneTopBar(
                 .clickable(onClick = onClickNavigationIcon),
             tint = SoptTheme.colors.onBackground
         )
+    }
+}
+
+@Preview
+@Composable
+fun FortuneTopBarPreview() {
+    SoptTheme {
+        FortuneTopBar {
+
+        }
     }
 }
