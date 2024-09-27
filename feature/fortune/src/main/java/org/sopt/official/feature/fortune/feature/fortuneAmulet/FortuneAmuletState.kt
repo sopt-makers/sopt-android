567e5ffb734029b53f8d1697cb461a62720491d9
Dongmin <pdm001125@naver.com>
2024-09-28 01:34:45 +0900

    feature #875: connect TodayFortuneCard api

---
+++ feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneAmulet/FortuneAmuletState.kt
@@ -0,0 +1,12 @@
+package org.sopt.official.feature.fortune.feature.fortuneAmulet
+
+import androidx.compose.ui.graphics.Color
+
+data class FortuneAmuletState(
+    val isLoading: Boolean = false,
+    val isFailure: Boolean = false,
+    val description: String = "",
+    val imageColor: Color = Color.White,
+    val imageUrl: String = "",
+    val name: String = "",
+)
