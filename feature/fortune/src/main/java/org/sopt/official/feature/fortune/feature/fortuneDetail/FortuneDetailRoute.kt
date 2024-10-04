f6c4312d5fba25b2b71887a87b49e60276887506
s9hn <tnghqn@naver.com>
2024-10-05 00:20:44 +0900

    feat: 외부 영역 터치 및 하단 스크롤 시 bottomsheet hide 기능 구현

--- feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneDetail/FortuneDetailRoute.kt
+++ feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneDetail/FortuneDetailRoute.kt
@@ -2,8 +2,10 @@ package org.sopt.official.feature.fortune.feature.fortuneDetail
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -12,12 +14,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.launch
+import org.sopt.official.designsystem.Gray800
 import org.sopt.official.feature.fortune.feature.fortuneDetail.component.PokeMessageBottomSheetScreen
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun FortuneDetailRoute(
     date: String,
@@ -27,39 +30,42 @@ internal fun FortuneDetailRoute(
     val context = LocalContext.current
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     var isAnonymous by remember { mutableStateOf(true) }
-    var isShowBottomSheet by remember { mutableStateOf(false) }
     var selectedIndex by remember { mutableIntStateOf(-1) }
-    val bottomSheetState = rememberModalBottomSheetState(confirmValueChange = { false })
+    val bottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope()
 
-    if (isShowBottomSheet) {
-        PokeMessageBottomSheetScreen(
-            sheetState = bottomSheetState,
-            onDismissRequest = { isShowBottomSheet = false },
-            selectedIndex = selectedIndex,
-            onItemClick = { newSelectedIndex, message ->
-                scope.launch {
-                    selectedIndex = newSelectedIndex
-                    viewModel.poke(message)
-                    bottomSheetState.hide()
-                }.invokeOnCompletion {
-                    if (!bottomSheetState.isVisible) isShowBottomSheet = false
-                }
-            },
-            onIconClick = { isAnonymous = !isAnonymous },
-            isAnonymous = isAnonymous,
-        )
-    }
-
-    FortuneDetailScreen(
-        date = date,
-        onFortuneAmuletClick = onFortuneAmuletClick,
-        onProfileClick = { userId ->
-            context.startActivity(
-                Intent(Intent.ACTION_VIEW, Uri.parse("https://playground.sopt.org/members/${userId}"))
+    ModalBottomSheetLayout(
+        sheetState = bottomSheetState,
+        sheetShape = RoundedCornerShape(
+            topStart = 20.dp,
+            topEnd = 20.dp,
+        ),
+        sheetBackgroundColor = Gray800,
+        sheetContent = {
+            PokeMessageBottomSheetScreen(
+                selectedIndex = selectedIndex,
+                onItemClick = { newSelectedIndex, message ->
+                    scope.launch {
+                        selectedIndex = newSelectedIndex
+                        bottomSheetState.hide()
+                        viewModel.poke(message)
+                    }
+                },
+                onIconClick = { isAnonymous = !isAnonymous },
+                isAnonymous = isAnonymous,
             )
         },
-        onPokeClick = { isShowBottomSheet = true },
-        uiState = uiState,
-    )
+    ) {
+        FortuneDetailScreen(
+            date = date,
+            onFortuneAmuletClick = onFortuneAmuletClick,
+            onProfileClick = { userId ->
+                context.startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://playground.sopt.org/members/${userId}"))
+                )
+            },
+            onPokeClick = { scope.launch { bottomSheetState.show() } },
+            uiState = uiState,
+        )
+    }
 }
--- feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneDetail/component/PokeMessageBottomSheet.kt
+++ feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneDetail/component/PokeMessageBottomSheet.kt
@@ -1,7 +1,6 @@
 package org.sopt.official.feature.fortune.feature.fortuneDetail.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,11 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,93 +24,76 @@ import androidx.compose.ui.unit.dp
 import okhttp3.internal.immutableListOf
 import org.sopt.official.designsystem.Gray10
 import org.sopt.official.designsystem.Gray30
-import org.sopt.official.designsystem.Gray800
 import org.sopt.official.designsystem.SoptTheme
 import org.sopt.official.feature.fortune.R.drawable.ic_checkbox_off
 import org.sopt.official.feature.fortune.R.drawable.ic_checkbox_on
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun PokeMessageBottomSheetScreen(
-    sheetState: SheetState,
-    onDismissRequest: () -> Unit,
     isAnonymous: Boolean,
     selectedIndex: Int,
     onItemClick: (selectedIndex: Int, message: String) -> Unit,
     onIconClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    ModalBottomSheet(
-        containerColor = Gray800,
-        sheetState = sheetState,
-        onDismissRequest = onDismissRequest,
-        dragHandle = null,
+    Column(
+        modifier = modifier.fillMaxWidth()
+            .padding(horizontal = 20.dp)
+            .padding(top = 24.dp, bottom = 12.dp),
     ) {
-        Column(
-            modifier = modifier.fillMaxWidth()
-                .padding(horizontal = 20.dp)
-                .padding(top = 24.dp, bottom = 12.dp)
-                .background(color = Gray800),
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth(),
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    text = "함께 보낼 메시지를 선택해주세요",
-                    style = SoptTheme.typography.heading20B,
-                    color = Gray30,
-                )
-                Spacer(modifier = Modifier.weight(weight = 1f))
-                Image(
-                    imageVector = if (isAnonymous) ImageVector.vectorResource(id = ic_checkbox_on)
-                    else ImageVector.vectorResource(id = ic_checkbox_off),
-                    contentDescription = "익명 체크박스",
-                    modifier = Modifier.clickable { onIconClick() }
-                )
-                Spacer(modifier = Modifier.width(width = 8.dp))
-                Text(
-                    text = "익명",
-                    style = SoptTheme.typography.title16SB,
-                    color = Gray10,
-                )
-            }
-            Spacer(modifier = Modifier.height(height = 12.dp))
-            LazyColumn(
-                contentPadding = PaddingValues(vertical = 4.dp),
-            ) {
-                val messages = immutableListOf(
-                    "안녕하세요? I AM 35기에요",
-                    "친해지고 싶어서 DM 드려요 ^^~",
-                    "이야기 해보고 싶었어요!!",
-                    "모각작 하실래요?",
-                    "콕 \uD83D\uDC48",
-                )
+            Text(
+                text = "함께 보낼 메시지를 선택해주세요",
+                style = SoptTheme.typography.heading20B,
+                color = Gray30,
+            )
+            Spacer(modifier = Modifier.weight(weight = 1f))
+            Image(
+                imageVector = if (isAnonymous) ImageVector.vectorResource(id = ic_checkbox_on)
+                else ImageVector.vectorResource(id = ic_checkbox_off),
+                contentDescription = "익명 체크박스",
+                modifier = Modifier.clickable { onIconClick() },
+            )
+            Spacer(modifier = Modifier.width(width = 8.dp))
+            Text(
+                text = "익명",
+                style = SoptTheme.typography.title16SB,
+                color = Gray10,
+            )
+        }
+        Spacer(modifier = Modifier.height(height = 12.dp))
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 4.dp),
+        ) {
+            val messages = immutableListOf(
+                "안녕하세요? I AM 35기에요",
+                "친해지고 싶어서 DM 드려요 ^^~",
+                "이야기 해보고 싶었어요!!",
+                "모각작 하실래요?",
+                "콕 \uD83D\uDC48",
+            )
 
-                itemsIndexed(messages) { index, message ->
-                    PokeMessageItem(
-                        message = message,
-                        isSelected = index == selectedIndex,
-                        onItemClick = { onItemClick(index, message) },
-                    )
-                }
+            itemsIndexed(messages) { index, message ->
+                PokeMessageItem(
+                    message = message,
+                    isSelected = index == selectedIndex,
+                    onItemClick = { onItemClick(index, message) },
+                )
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 private fun PokeMessageBottomSheetScreenPreview() {
     SoptTheme {
         PokeMessageBottomSheetScreen(
-            sheetState = rememberModalBottomSheetState(
-                confirmValueChange = { true },
-            ),
-            onDismissRequest = { },
-            isAnonymous = false,
+            isAnonymous = true,
             selectedIndex = 0,
             onItemClick = { _, _ -> },
             onIconClick = { },
