9b3a44a6a536d27c9efd2521467b63e3fa69ca34
s9hn <tnghqn@naver.com>
2024-10-07 17:42:48 +0900

    refactor: Icon으로 수정

--- feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneDetail/component/PokeSnackBar.kt
+++ feature/fortune/src/main/java/org/sopt/official/feature/fortune/feature/fortuneDetail/component/PokeSnackBar.kt
@@ -1,6 +1,5 @@
 package org.sopt.official.feature.fortune.feature.fortuneDetail.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.res.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.designsystem.Gray10
@@ -34,8 +35,9 @@ internal fun PokeSnackBar() {
             )
     ) {
         Spacer(modifier = Modifier.width(width = 16.dp))
-        Image(
+        Icon(
             imageVector = ImageVector.vectorResource(ic_alert),
+            tint = Color.Unspecified,
             contentDescription = null,
         )
         Spacer(modifier = Modifier.width(width = 8.dp))
