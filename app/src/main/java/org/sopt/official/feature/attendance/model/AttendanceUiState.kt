e3aa873027ada48755e480c81b32045c9f89bff9
giovannijunseokim <kingjunseo@gmail.com>
2024-10-18 17:56:48 +0900

    chore: remove mock

--- app/src/main/java/org/sopt/official/feature/attendance/model/AttendanceUiState.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/model/AttendanceUiState.kt
@@ -1,9 +1,16 @@
 package org.sopt.official.feature.attendance.model
 
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableMap
+
 sealed interface AttendanceUiState {
     data object Loading : AttendanceUiState
     data class Success(
-        val fakeTitle: String
+        val attendanceDayType: AttendanceDayType,
+        val userTitle: String,
+        val attendanceScore: Int,
+        val totalAttendanceResult: ImmutableMap<AttendanceResultType, Int>,
+        val attendanceHistoryList: ImmutableList<AttendanceHistory>,
     ) : AttendanceUiState
 
     data class Failure(val error: Throwable?) : AttendanceUiState
