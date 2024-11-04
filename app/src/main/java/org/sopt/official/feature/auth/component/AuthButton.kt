b681ad6cdbaaaf216fc622ba3bcc922e7c066228
LEE YOU BIN <youbeene3409@naver.com>
2024-11-04 11:23:21 +0900

    [feature/#928] add auth button file

---
+++ app/src/main/java/org/sopt/official/feature/auth/component/AuthButton.kt
@@ -0,0 +1,16 @@
+package org.sopt.official.feature.auth.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AuthButton(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+
+    }
+}
