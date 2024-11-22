12f5912f27955397f474ee5bf857c50b86eb545d
Dongmin <pdm001125@naver.com>
2024-11-22 16:54:57 +0900

    feat #976: feat SoptlogProfile

---
+++ feature/soptlog/src/main/java/org/sopt/official/feature/soptlog/component/SoptlogProfile.kt
@@ -0,0 +1,78 @@
+package org.sopt.official.feature.soptlog.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
+import coil3.test.FakeImage
+import org.sopt.official.designsystem.SoptTheme
+
+@Composable
+fun SoptlogProfile(
+    profileImageUrl: String,
+    name: String,
+    part: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = profileImageUrl,
+            contentDescription = "Profile Image",
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text(
+                text = name,
+                style = SoptTheme.typography.heading20B,
+                color = SoptTheme.colors.surface
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(
+                text = part,
+                style = SoptTheme.typography.body13M,
+                color = SoptTheme.colors.onSurface100
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Preview
+@Composable
+fun PreviewSoptlogProfile() {
+    SoptTheme {
+        val previewHandler = AsyncImagePreviewHandler {
+            FakeImage(color = 0xFFE0E0E0.toInt())
+        }
+
+        CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+            SoptlogProfile(
+                "https://avatars.githubusercontent.com/u/48426991?v=4",
+                "차은우",
+                "안드로이드"
+            )
+        }
+    }
+}
