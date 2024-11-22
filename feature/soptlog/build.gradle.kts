60e4cb8ac4736117d8d8b02e308e5e62834c9b5e
Dongmin Park <pdm001125@naver.com>
2024-11-22 17:36:37 +0900

    [Feature/#976] soptlog module (#979)
    * Strong skipping mode 설정 삭제
    
    * feat #976: create module
    
    * feat #976: add coil3 library
    
    * feat #976: feat SoptlogIntroduction
    
    * feat #976: feat SoptlogProfile
    
    * feat #976: feat SoptlogScreen TopBar, Profile, Introduction
    
    * feat #976: background color 옵션에 넣기
    
    * feat #976: UrlImage 공통 모듈 추출 및 적용

---
+++ feature/soptlog/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    sopt("feature")
+    sopt("compose")
+    sopt("test")
+    sopt("deeplink")
+}
+
+android {
+    namespace = "org.sopt.official.feature.soptlog"
+}
+
+ksp {
+    arg("deepLink.incremental", "true")
+    arg("deepLink.customAnnotations", "com.airbnb.AppDeepLink|com.airbnb.WebDeepLink")
+}
+
+dependencies {
+    // core
+    implementation(projects.core.common)
+    implementation(projects.core.designsystem)
+
+    // etc
+    implementation(libs.coil3.compose)
+    implementation(libs.coil3.test)
+}
