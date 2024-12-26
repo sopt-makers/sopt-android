1407f8e17f7df6118222f438bc060a555320e66f
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-12-26 19:17:25 +0900

    [#1009] TrackerModule에 구현체 주입

--- core/analytics/src/main/java/org/sopt/official/analytics/TrackerModule.kt
+++ core/analytics/src/main/java/org/sopt/official/analytics/TrackerModule.kt
@@ -1,4 +1,16 @@
 package org.sopt.official.analytics
 
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.official.analytics.impl.AmplitudeTracker
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 interface TrackerModule {
+    @Binds
+    @Singleton
+    fun bindAmplitudeTracker(amplitudeTracker: AmplitudeTracker): Tracker
 }
--- core/analytics/src/main/java/org/sopt/official/analytics/TrackerType.kt
+++
@@ -1,3 +0,0 @@
-package org.sopt.official.analytics
-
-annotation class TrackerType()
