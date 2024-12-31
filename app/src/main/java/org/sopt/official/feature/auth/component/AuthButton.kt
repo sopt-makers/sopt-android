2f9cefbf92d077085d80901545585ce9be8dae9f
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-12-31 20:30:55 +0900

    [feature/#1011] Modify AuthButton, AuthTextField

--- app/src/main/java/org/sopt/official/feature/auth/component/AuthButton.kt
+++ app/src/main/java/org/sopt/official/feature/auth/component/AuthButton.kt
@@ -28,14 +28,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.official.designsystem.Black
 import org.sopt.official.designsystem.Black80
 import org.sopt.official.designsystem.Gray60
+import org.sopt.official.designsystem.SoptTheme
 import org.sopt.official.designsystem.White
 
 @Composable
@@ -44,9 +47,9 @@ internal fun AuthButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(10.dp),
+    isEnabled: Boolean = true,
     containerColor: Color = White,
     contentColor: Color = Black,
-    isEnabled: Boolean = true,
     disabledContainerColor: Color = Black80,
     disabledContentColor: Color = Gray60,
     content: @Composable () -> Unit,
@@ -65,5 +68,18 @@ internal fun AuthButton(
         )
     ) {
         content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AuthButtonPreview() {
+    SoptTheme {
+        AuthButton(
+            padding = PaddingValues(16.dp),
+            onClick = {},
+        ) {
+            Text("로그인")
+        }
     }
 }
