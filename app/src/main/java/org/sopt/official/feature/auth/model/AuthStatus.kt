3ce6f69848baeb943ba21ff840331804c67de2f7
LEE YOU BIN <youbeene3409@naver.com>
2025-01-02 10:02:46 +0900

    [feature/#1011] 핸드폰 인증 검사 서버통신 모든 케이스 구현

--- app/src/main/java/org/sopt/official/feature/auth/model/AuthStatus.kt
+++ app/src/main/java/org/sopt/official/feature/auth/model/AuthStatus.kt
@@ -1,7 +1,28 @@
 package org.sopt.official.feature.auth.model
 
-enum class AuthStatus(val type: String) {
-    REGISTER("REGISTER"),
-    CHANGE("CHANGE"),
-    SEARCH("SEARCH")
+// TODO: mock서버 삭제 시 authName, phone, code는 같이 없어질 예정입니다.
+enum class AuthStatus(
+    val authName: String,
+    val phone: String,
+    val type: String,
+    val code: String
+) {
+    REGISTER(
+        authName = "Mock-Success-Register",
+        phone = "01012345678",
+        type = "REGISTER",
+        code = "123456"
+    ),
+    CHANGE(
+        authName = "Mock-Success-Change",
+        phone = "01087654321",
+        type = "CHANGE",
+        code = "654321"
+    ),
+    SEARCH(
+        authName = "Mock-Success-Search",
+        phone = "01013245768",
+        type = "SEARCH",
+        code = "132456"
+    )
 }
