8589588df8b3a24c3df5cd01d23566a7074af451
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-01-04 02:40:35 +0900

    [#1011] SERVER_CLIENT_ID 추가

--- build-logic/convention/src/main/kotlin/org/sopt/official/plugin/CommonConfigs.kt
+++ build-logic/convention/src/main/kotlin/org/sopt/official/plugin/CommonConfigs.kt
@@ -38,6 +38,7 @@ internal fun Project.configureAndroidCommonPlugin() {
             val userStatusKeyAlias = properties["userStatusKeyAlias"] as? String ?: ""
             val pushTokenKeyAlias = properties["pushTokenKeyAlias"] as? String ?: ""
             val mockAuthApi = properties["mockAuthApi"] as? String ?: ""
+            val serverClientId = properties["serverClientId"] as? String ?: ""
             buildConfigField("String", "SOPTAMP_API_KEY", apiKey)
             buildConfigField("String", "SOPTAMP_DATA_STORE_KEY", dataStoreKey)
             buildConfigField("String", "POKE_DATA_STORE_KEY", pokeDataStoreKey)
@@ -53,6 +54,7 @@ internal fun Project.configureAndroidCommonPlugin() {
             buildConfigField("String", "USER_STATUS_KEY_ALIAS", userStatusKeyAlias)
             buildConfigField("String", "PUSH_TOKEN_KEY_ALIAS", pushTokenKeyAlias)
             buildConfigField("String", "MOCK_AUTH_API", mockAuthApi)
+            buildConfigField("String", "SERVER_CLIENT_ID", serverClientId)
         }
         buildFeatures.apply {
             viewBinding = true
