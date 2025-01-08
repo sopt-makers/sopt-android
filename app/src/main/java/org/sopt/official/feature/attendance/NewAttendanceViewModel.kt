274dddaa9bb85ebb790984cefad6b43f7f0c685c
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-01-09 04:24:34 +0900

    chore: make code clear

--- app/src/main/java/org/sopt/official/feature/attendance/NewAttendanceViewModel.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/NewAttendanceViewModel.kt
@@ -3,24 +3,13 @@ package org.sopt.official.feature.attendance
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.collections.immutable.persistentMapOf
-import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.sopt.official.domain.entity.attendance.Attendance
-import org.sopt.official.domain.entity.attendance.Attendance.AttendanceDayType.HasAttendance.RoundAttendance.RoundAttendanceState
 import org.sopt.official.domain.repository.attendance.NewAttendanceRepository
 import org.sopt.official.feature.attendance.model.AttendanceUiState
-import org.sopt.official.feature.attendance.model.AttendanceUiState.Success.AttendanceDayType
-import org.sopt.official.feature.attendance.model.AttendanceUiState.Success.AttendanceDayType.AttendanceDay.MidtermAttendance
-import org.sopt.official.feature.attendance.model.AttendanceUiState.Success.AttendanceDayType.AttendanceDay.MidtermAttendance.NotYet.AttendanceSession
-import org.sopt.official.feature.attendance.model.AttendanceUiState.Success.AttendanceHistory
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.format.TextStyle
-import java.util.Locale
 import javax.inject.Inject
 
 
@@ -36,103 +25,12 @@ class NewAttendanceViewModel @Inject constructor(
     private val _uiState: MutableStateFlow<AttendanceUiState> = MutableStateFlow(AttendanceUiState.Loading)
     val uiState: StateFlow<AttendanceUiState> = _uiState
 
-
     fun fetchAttendanceInfo() {
         viewModelScope.launch {
             val attendance: Attendance = attendanceRepository.fetchAttendanceInfo()
             _uiState.update {
-                AttendanceUiState.Success(
-                    attendanceDayType = when (attendance.attendanceDayType) {
-                        is Attendance.AttendanceDayType.HasAttendance -> {
-                            val session = attendance.attendanceDayType.session
-                            val firstRoundAttendance = attendance.attendanceDayType.firstRoundAttendance
-                            val secondRoundAttendance = attendance.attendanceDayType.secondRoundAttendance
-
-                            AttendanceDayType.AttendanceDay(
-                                eventDate = formatSessionTime(session.startAt, session.endAt),
-                                eventLocation = session.location ?: "장소 정보를 불러올 수 없습니다.",
-                                eventName = session.name,
-                                firstAttendance = when (firstRoundAttendance.state) {
-                                    RoundAttendanceState.ATTENDANCE -> MidtermAttendance.Present(
-                                        attendanceAt = firstRoundAttendance.attendedAt.toString()
-                                    )
-
-                                    RoundAttendanceState.NOT_YET -> MidtermAttendance.NotYet(
-                                        attendanceSession = AttendanceSession.FIRST
-                                    )
-
-                                    RoundAttendanceState.ABSENT -> MidtermAttendance.Absent
-                                },
-                                secondAttendance = when (secondRoundAttendance.state) {
-                                    RoundAttendanceState.ATTENDANCE -> MidtermAttendance.Present(
-                                        attendanceAt = secondRoundAttendance.attendedAt.toString()
-                                    )
-
-
-                                    RoundAttendanceState.NOT_YET -> MidtermAttendance.NotYet(
-                                        attendanceSession = AttendanceSession.FIRST
-                                    )
-
-                                    RoundAttendanceState.ABSENT -> MidtermAttendance.Absent
-                                },
-                                finalAttendance = run {
-                                    if (secondRoundAttendance.state == RoundAttendanceState.NOT_YET) AttendanceDayType.AttendanceDay.FinalAttendance.NOT_YET
-                                    if (firstRoundAttendance.state == RoundAttendanceState.ABSENT && secondRoundAttendance.state == RoundAttendanceState.ABSENT) AttendanceDayType.AttendanceDay.FinalAttendance.ABSENT
-                                    if (firstRoundAttendance.state == RoundAttendanceState.ABSENT || secondRoundAttendance.state == RoundAttendanceState.ABSENT) AttendanceDayType.AttendanceDay.FinalAttendance.LATE
-                                    else AttendanceDayType.AttendanceDay.FinalAttendance.PRESENT
-                                }
-                            )
-                        }
-
-                        is Attendance.AttendanceDayType.NoAttendance -> {
-                            val session = attendance.attendanceDayType.session
-                            AttendanceDayType.Event(
-                                eventDate = formatSessionTime(session.startAt, session.endAt),
-                                eventLocation = session.location ?: "장소 정보를 불러올 수 없습니다.",
-                                eventName = session.name
-                            )
-                        }
-
-                        is Attendance.AttendanceDayType.NoSession -> {
-                            AttendanceDayType.None
-                        }
-                    },
-                    userTitle = "${attendance.user.generation}기 ${attendance.user.part.partName}파트 ${attendance.user.name}",
-                    attendanceScore = attendance.user.attendanceScore.toFloat(),
-                    totalAttendanceResult = persistentMapOf(
-                        AttendanceUiState.Success.AttendanceResultType.ALL to attendance.user.attendanceCount.totalCount,
-                        AttendanceUiState.Success.AttendanceResultType.PRESENT to attendance.user.attendanceCount.attendanceCount,
-                        AttendanceUiState.Success.AttendanceResultType.LATE to attendance.user.attendanceCount.lateCount,
-                        AttendanceUiState.Success.AttendanceResultType.ABSENT to attendance.user.attendanceCount.absenceCount,
-                    ),
-                    attendanceHistoryList = attendance.user.attendanceHistory.map { attendanceLog: Attendance.User.AttendanceLog ->
-                        AttendanceHistory(
-                            status = when (attendanceLog.attendanceState) {
-                                Attendance.User.AttendanceLog.AttendanceState.PARTICIPATE -> "참여"
-                                Attendance.User.AttendanceLog.AttendanceState.ATTENDANCE -> "출석"
-                                Attendance.User.AttendanceLog.AttendanceState.TARDY -> "지각"
-                                Attendance.User.AttendanceLog.AttendanceState.ABSENT -> "결석"
-                            },
-                            eventName = attendanceLog.sessionName,
-                            date = attendanceLog.date
-                        )
-                    }.toPersistentList()
-                )
+                AttendanceUiState.Success.of(attendance)
             }
         }
-    }
-
-    private fun formatSessionTime(startAt: LocalDateTime, endAt: LocalDateTime): String {
-        val dateFormatter = DateTimeFormatter.ofPattern("M월 d일")
-        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
-
-        return "${startAt.format(dateFormatter)} ${startAt.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.KOREAN)} ${
-            startAt.format(
-                timeFormatter
-            )
-        } - " + if (startAt.toLocalDate() == endAt.toLocalDate()) endAt.format(timeFormatter)
-        else "${endAt.format(dateFormatter)} ${
-            endAt.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.KOREAN)
-        } ${endAt.format(timeFormatter)}"
     }
 }
--- app/src/main/java/org/sopt/official/feature/attendance/model/AttendanceUiState.kt
+++ app/src/main/java/org/sopt/official/feature/attendance/model/AttendanceUiState.kt
@@ -1,9 +1,11 @@
 package org.sopt.official.feature.attendance.model
 
-import androidx.annotation.DrawableRes
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableMap
-import org.sopt.official.R
+import kotlinx.collections.immutable.toPersistentList
+import org.sopt.official.domain.entity.attendance.Attendance
+import org.sopt.official.feature.attendance.toTotalAttendanceResult
+import org.sopt.official.feature.attendance.toUiAttendanceDayType
 
 sealed interface AttendanceUiState {
     data object Loading : AttendanceUiState
@@ -14,83 +16,6 @@ sealed interface AttendanceUiState {
         val totalAttendanceResult: ImmutableMap<AttendanceResultType, Int>,
         val attendanceHistoryList: ImmutableList<AttendanceHistory>,
     ) : AttendanceUiState {
-        sealed interface AttendanceDayType {
-            /** 출석이 진행되는 날 **/
-            data class AttendanceDay(
-                val eventDate: String,
-                val eventLocation: String,
-                val eventName: String,
-                val firstAttendance: MidtermAttendance,
-                val secondAttendance: MidtermAttendance,
-                val finalAttendance: FinalAttendance,
-            ) : AttendanceDayType {
-                sealed class MidtermAttendance private constructor(
-                    @DrawableRes val imageResId: Int,
-                    val isFinished: Boolean,
-                    val description: String,
-                ) {
-                    data class NotYet(val attendanceSession: AttendanceSession) : MidtermAttendance(
-                        imageResId = R.drawable.ic_attendance_state_nothing,
-                        isFinished = false,
-                        description = attendanceSession.type
-                    ) {
-                        enum class AttendanceSession(val type: String) {
-                            FIRST("1차 출석"),
-                            SECOND("2차 출석")
-                        }
-                    }
-
-                    data class Present(val attendanceAt: String) : MidtermAttendance(
-                        imageResId = R.drawable.ic_attendance_state_yes,
-                        isFinished = true,
-                        description = attendanceAt
-                    )
-
-                    data object Absent : MidtermAttendance(
-                        imageResId = R.drawable.ic_attendance_state_absence_white,
-                        isFinished = true,
-                        description = "-"
-                    )
-                }
-
-                enum class FinalAttendance(
-                    @DrawableRes val imageResId: Int,
-                    val isFinished: Boolean,
-                    val result: String,
-                ) {
-                    NOT_YET(
-                        imageResId = R.drawable.ic_attendance_state_nothing,
-                        isFinished = false,
-                        result = "출석 전"
-                    ),
-                    PRESENT(
-                        imageResId = R.drawable.ic_attendance_state_done,
-                        isFinished = true,
-                        result = "출석완료!"
-                    ),
-                    LATE(
-                        imageResId = R.drawable.ic_attendance_state_late,
-                        isFinished = true,
-                        result = "지각"
-                    ),
-                    ABSENT(
-                        imageResId = R.drawable.ic_attendance_state_absence_black,
-                        isFinished = true,
-                        result = "결석"
-                    )
-                }
-            }
-
-            /** 출석할 필요가 없는 날 **/
-            data class Event(
-                val eventDate: String,
-                val eventLocation: String,
-                val eventName: String,
-            ) : AttendanceDayType
-
-            /** 아무 일정이 없는 날 **/
-            data object None : AttendanceDayType
-        }
 
         enum class AttendanceResultType(val type: String) {
             ALL(type = "전체"),
@@ -104,6 +29,29 @@ sealed interface AttendanceUiState {
             val eventName: String,
             val date: String,
         )
+
+        companion object {
+            fun of(attendance: Attendance): Success {
+                return Success(
+                    attendanceDayType = attendance.attendanceDayType.toUiAttendanceDayType(),
+                    userTitle = "${attendance.user.generation}기 ${attendance.user.part.partName}파트 ${attendance.user.name}",
+                    attendanceScore = attendance.user.attendanceScore.toFloat(),
+                    totalAttendanceResult = attendance.user.attendanceCount.toTotalAttendanceResult(),
+                    attendanceHistoryList = attendance.user.attendanceHistory.map { attendanceLog: Attendance.User.AttendanceLog ->
+                        AttendanceHistory(
+                            status = when (attendanceLog.attendanceState) {
+                                Attendance.User.AttendanceLog.AttendanceState.PARTICIPATE -> "참여"
+                                Attendance.User.AttendanceLog.AttendanceState.ATTENDANCE -> "출석"
+                                Attendance.User.AttendanceLog.AttendanceState.TARDY -> "지각"
+                                Attendance.User.AttendanceLog.AttendanceState.ABSENT -> "결석"
+                            },
+                            eventName = attendanceLog.sessionName,
+                            date = attendanceLog.date
+                        )
+                    }.toPersistentList()
+                )
+            }
+        }
     }
 
     data class Failure(val error: Throwable?) : AttendanceUiState
