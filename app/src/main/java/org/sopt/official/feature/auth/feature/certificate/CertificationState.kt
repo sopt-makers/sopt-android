da75b4e6edcba44306edd642b255a96d277c8a27
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-01-17 00:52:25 +0900

    [#1011] 타이머 이슈 해소

--- app/src/main/java/org/sopt/official/feature/auth/feature/certificate/CertificationState.kt
+++ app/src/main/java/org/sopt/official/feature/auth/feature/certificate/CertificationState.kt
@@ -1,7 +1,17 @@
 package org.sopt.official.feature.auth.feature.certificate
 
+import java.util.Locale
+
 data class CertificationState(
     val phone: String = "",
     val code: String = "",
-    val currentTime: String = "03:00"
-)
+    val currentTimeValue: Int = 180
+) {
+    val currentTime: String
+        get() = String.format(
+            Locale.US,
+            "%02d:%02d",
+            currentTimeValue / 60,
+            currentTimeValue % 60
+        )
+}
