05c4cd161108f84ed2a166e4c6473b7fbb8d549a
LEE YOU BIN <youbeene3409@naver.com>
2025-01-16 20:13:57 +0900

    [feature/#1011] 타이머 로직 구현

--- app/src/main/java/org/sopt/official/feature/auth/feature/certificate/CertificationViewModel.kt
+++ app/src/main/java/org/sopt/official/feature/auth/feature/certificate/CertificationViewModel.kt
@@ -3,21 +3,30 @@ package org.sopt.official.feature.auth.feature.certificate
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.sopt.official.domain.auth.model.InformationWithCode
 import org.sopt.official.domain.auth.model.InitialInformation
 import org.sopt.official.domain.auth.model.UserPhoneNumber
 import org.sopt.official.domain.auth.repository.AuthRepository
 import org.sopt.official.feature.auth.model.AuthStatus
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class CertificationViewModel @Inject constructor(
     private val repository: AuthRepository
 ) : ViewModel() {
+
+    private val _state = MutableStateFlow(CertificationState())
+    val state: StateFlow<CertificationState> = _state.asStateFlow()
 
     private val _sideEffect = MutableSharedFlow<CertificationSideEffect>()
     val sideEffect: SharedFlow<CertificationSideEffect> = _sideEffect.asSharedFlow()
@@ -69,6 +78,29 @@ class CertificationViewModel @Inject constructor(
             }.onFailure {
                 _sideEffect.emit(CertificationSideEffect.ShowToast("실패ㅠㅠ"))
             }
+        }
+    }
+
+    fun startTimer(currentTime: String) {
+        viewModelScope.launch {
+            var (minutes, seconds) = currentTime.split(":").map { it.toInt() }
+            var totalSeconds: Int = minutes * 60 + seconds
+
+            while (totalSeconds > 0) {
+                delay(1000L)
+
+                totalSeconds--
+                minutes = totalSeconds / 60
+                seconds = totalSeconds % 60
+
+                _state.update { currentState ->
+                    currentState.copy(
+                        currentTime = String.format(Locale.US, "%02d:%02d", minutes, seconds)
+                    )
+                }
+            }
+            delay(1000L)
+            // todo: 에러 화면 띄우기
         }
     }
 }
