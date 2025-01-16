768c07bf31c41217c681b04e284f7e08c6849cd1
LEE YOU BIN <youbeene3409@naver.com>
2025-01-16 20:37:47 +0900

    [feature/#1011] phone, code state로 관리

--- app/src/main/java/org/sopt/official/feature/auth/feature/certificate/CertificationState.kt
+++ app/src/main/java/org/sopt/official/feature/auth/feature/certificate/CertificationState.kt
@@ -1,5 +1,7 @@
 package org.sopt.official.feature.auth.feature.certificate
 
 data class CertificationState(
+    val phone: String = "",
+    val code: String = "",
     val currentTime: String = "03:00"
 )
