f2514e3ddadb438db5804b9b25eff8aa5d26e787
Dongmin <pdm001125@naver.com>
2025-02-08 14:45:01 +0900

    feat #1033: apply screen

--- feature/schedule/src/main/java/com/sopt/official/feature/schedule/ScheduleActivity.kt
+++ feature/schedule/src/main/java/com/sopt/official/feature/schedule/ScheduleActivity.kt
@@ -6,19 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
@@ -28,7 +23,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,6 +31,8 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sopt.official.feature.schedule.component.ScheduleItem
+import com.sopt.official.feature.schedule.component.VerticalDividerWithCircle
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.official.designsystem.SoptTheme
 
@@ -105,81 +101,14 @@ fun ScheduleScreen() {
                 contentPadding = PaddingValues(vertical = 16.dp)
             ) {
                 items(fakeData) { item ->
-                    Row {
-                        Box(
-                            contentAlignment = Alignment.TopCenter,
-                        ) {
-                            VerticalDivider(
-                                modifier = Modifier
-                                    .height(100.dp)
-                                    .width(1.dp),
-                                color = SoptTheme.colors.onSurface500
-                            )
-                            Box(
-                                modifier = Modifier
-                                    .size(16.dp)
-                                    .background(
-                                        SoptTheme.colors.onSurface500,
-                                        CircleShape
-                                    )
-                            )
-                        }
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Column(
-                            modifier = Modifier
-                                .padding(vertical = 12.dp)
-                                .fillMaxWidth()
-                        ) {
-                            Text(
-                                text = item.first,
-                                color = SoptTheme.colors.primary,
-                                style = SoptTheme.typography.body14M,
-                            )
-
-                            Spacer(modifier = Modifier.height(8.dp))
-
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(
-                                    text = "세미나",
-                                    color = SoptTheme.colors.primary,
-                                    modifier = Modifier
-                                        .background(SoptTheme.colors.error, RoundedCornerShape(4.dp)) // 임시 색상 입니다
-                                        .padding(horizontal = 6.dp, vertical = 3.dp),
-                                    style = SoptTheme.typography.label11SB,
-                                )
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(
-                                    text = item.second,
-                                    color = SoptTheme.colors.onSurface10, // 임시 색상 입니다
-                                    style = SoptTheme.typography.heading18B
-                                )
-                            }
-                        }
-
-                    }
+                    ScheduleItem(
+                        date = item.first,
+                        event = item.second
+                    )
                 }
 
                 item {
-                    Box(
-                        contentAlignment = Alignment.TopCenter,
-                    ) {
-                        VerticalDivider(
-                            modifier = Modifier
-                                .height(100.dp)
-                                .width(1.dp),
-                            color = SoptTheme.colors.onSurface500
-                        )
-                        Box(
-                            modifier = Modifier
-                                .size(16.dp)
-                                .background(
-                                    Color.Unspecified,
-                                    CircleShape
-                                )
-                        )
-                    }
+                    VerticalDividerWithCircle(Color.Unspecified)
                 }
             }
 
