25747cb78d69cb7274a60cad0bb4178ec35f84cc
s9hn <tnghqn@naver.com>
2025-02-21 02:26:52 +0900

    refactor: errorDialog, LoadingIndicator 분리

--- feature/home/src/main/java/org/sopt/official/feature/home/component/HomeErrorDialog.kt
+++ feature/home/src/main/java/org/sopt/official/feature/home/component/HomeErrorDialog.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -38,6 +39,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,50 +54,57 @@ internal fun HomeErrorDialog(
     onCheckClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Dialog(onDismissRequest = onCheckClick) {
-        Box(
-            modifier = modifier
-                .fillMaxWidth()
-                .background(
-                    color = colors.onBackground,
-                    shape = RoundedCornerShape(size = 10.dp),
-                )
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
+    Box(
+        contentAlignment = Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colors.background.copy(alpha = 0.55f)),
+    ) {
+        Dialog(onDismissRequest = onCheckClick) {
+            Box(
+                modifier = modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 8.dp),
-            ) {
-                Spacer(modifier = Modifier.height(height = 26.dp))
-                Text(
-                    text = "네트워크가 원활하지 않습니다.",
-                    style = typography.heading16B,
-                    color = colors.onSurface800,
-                )
-                Spacer(modifier = Modifier.height(height = 24.dp))
-                Text(
-                    text = "인터넷 연결을 확인하고 다시 시도해 주세요.",
-                    style = typography.body14M,
-                    color = colors.onSurface100,
-                )
-                Spacer(modifier = Modifier.height(height = 34.dp))
-                Button(
-                    onClick = onCheckClick,
-                    shape = RoundedCornerShape(size = 10.dp),
-                    contentPadding = PaddingValues(
-                        horizontal = 128.dp,
-                        vertical = 12.dp,
-                    ),
-                    colors = ButtonDefaults.buttonColors(containerColor = Black),
-                ) {
-                    Text(
-                        text = "확인",
-                        style = typography.body14R,
-                        color = colors.onSurface10,
+                    .background(
+                        color = colors.onBackground,
+                        shape = RoundedCornerShape(size = 10.dp),
                     )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp),
+                ) {
+                    Spacer(modifier = Modifier.height(height = 26.dp))
+                    Text(
+                        text = "네트워크가 원활하지 않습니다.",
+                        style = typography.heading16B,
+                        color = colors.onSurface800,
+                    )
+                    Spacer(modifier = Modifier.height(height = 24.dp))
+                    Text(
+                        text = "인터넷 연결을 확인하고 다시 시도해 주세요.",
+                        style = typography.body14M,
+                        color = colors.onSurface100,
+                    )
+                    Spacer(modifier = Modifier.height(height = 34.dp))
+                    Button(
+                        onClick = onCheckClick,
+                        shape = RoundedCornerShape(size = 10.dp),
+                        contentPadding = PaddingValues(
+                            horizontal = 128.dp,
+                            vertical = 12.dp,
+                        ),
+                        colors = ButtonDefaults.buttonColors(containerColor = Black),
+                    ) {
+                        Text(
+                            text = "확인",
+                            style = typography.body14R,
+                            color = colors.onSurface10,
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(height = 10.dp))
                 }
-                Spacer(modifier = Modifier.height(height = 10.dp))
             }
         }
     }
