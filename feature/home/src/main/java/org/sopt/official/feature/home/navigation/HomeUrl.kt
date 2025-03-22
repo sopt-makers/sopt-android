5757418aacd346bb5457c13769452a4ff6b223cb
MinJae Han <hanminjae131@gmail.com>
2025-03-23 03:43:54 +0900

    [MOD/#1113] add UNKNOWN to HomeUrl enum class
    Adds an `UNKNOWN` entry to the `HomeUrl` enum class with an empty string URL. Modifies the `from` function to return `UNKNOWN` instead of `null` when the input URL does not match any existing entries.

--- feature/home/src/main/java/org/sopt/official/feature/home/navigation/HomeUrl.kt
+++ feature/home/src/main/java/org/sopt/official/feature/home/navigation/HomeUrl.kt
@@ -3,12 +3,12 @@ package org.sopt.official.feature.home.navigation
 enum class HomeUrl(val url: String) {
     POKE("home/poke"),
     FORTUNE("home/fortune"),
-    SOPTAMP("home/soptamp");
+    SOPTAMP("home/soptamp"),
+    UNKNOWN("");
 
     companion object {
-        fun from(url: String?): HomeUrl? {
-            if (url == null) return null
-            return entries.find { it.url == url }
+        fun from(url: String?): HomeUrl {
+            return entries.find { it.url == url } ?: UNKNOWN
         }
     }
 }
