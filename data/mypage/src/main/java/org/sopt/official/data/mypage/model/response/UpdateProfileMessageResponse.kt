2021d12b014ea7325d7b22318718fd95d3178a46
Hyobeen-Park <amanda0109@naver.com>
2025-04-14 02:02:11 +0900

    [FIX/#1160] 한 마디 편집 관련 dto keep 어노테이션 추가

--- data/mypage/src/main/java/org/sopt/official/data/mypage/model/response/UpdateProfileMessageResponse.kt
+++ data/mypage/src/main/java/org/sopt/official/data/mypage/model/response/UpdateProfileMessageResponse.kt
@@ -24,9 +24,11 @@
  */
 package org.sopt.official.data.mypage.model.response
 
+import androidx.annotation.Keep
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Keep
 @Serializable
 data class UpdateProfileMessageResponse(
     @SerialName("profileMessage")
