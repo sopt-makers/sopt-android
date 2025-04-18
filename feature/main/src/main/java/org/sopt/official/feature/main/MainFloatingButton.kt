e91235238e771b655585a949aa8f6fcec8703be2
Hyobeen-Park <amanda0109@naver.com>
2025-04-18 21:45:03 +0900

    [FEAT/#1162] 딤 애니메이션 추가, 메뉴 클릭 시 리플 제거

--- feature/main/src/main/java/org/sopt/official/feature/main/MainFloatingButton.kt
+++ feature/main/src/main/java/org/sopt/official/feature/main/MainFloatingButton.kt
@@ -10,12 +10,14 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -64,6 +66,11 @@ internal fun MainFloatingButton(
         animationSpec = tween(durationMillis = 400, easing = FastOutSlowInEasing)
     )
 
+    val backgroundAlpha by animateFloatAsState(
+        targetValue = if (isFloatingButtonClicked) 0.65f else 0f,
+        animationSpec = tween(durationMillis = 600, easing = FastOutSlowInEasing)
+    )
+
     Box(
         contentAlignment = Alignment.BottomCenter,
         modifier = Modifier
@@ -75,7 +82,7 @@ internal fun MainFloatingButton(
                 modifier = Modifier
                     .fillMaxSize()
                     .then(
-                        if (isFloatingButtonClicked) Modifier.background(SoptTheme.colors.onSurface.copy(0.65f))
+                        if (isFloatingButtonClicked) Modifier.background(SoptTheme.colors.onSurface.copy(backgroundAlpha))
                         else Modifier
                     )
                     .clickable {
@@ -150,7 +157,10 @@ private fun FloatingMenuItem(
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
-                    .clickable {
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    ) {
                         context.startActivity(getIntent(weblink, context))
                     }
             ) {
@@ -165,6 +175,7 @@ private fun FloatingMenuItem(
                     color = SoptTheme.colors.onSurface950,
                     modifier = Modifier
                         .padding(6.dp)
+                        .fillMaxWidth()
                 )
             }
         }
