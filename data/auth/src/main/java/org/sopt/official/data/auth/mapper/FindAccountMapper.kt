9a19f4f13627466b181161f0029f0265290b3b22
LEE YOU BIN <youbeene3409@naver.com>
2025-06-09 22:52:19 +0900

    [feat/#1178] 전화번호 수정 관련 코드 삭제

--- data/auth/src/main/java/org/sopt/official/data/auth/mapper/FindAccountMapper.kt
+++ data/auth/src/main/java/org/sopt/official/data/auth/mapper/FindAccountMapper.kt
@@ -24,15 +24,8 @@
  */
 package org.sopt.official.data.auth.mapper
 
-import org.sopt.official.data.auth.remote.request.FindAccountRequest
 import org.sopt.official.data.auth.remote.response.FindAccountResponse
 import org.sopt.official.domain.auth.model.Auth
-import org.sopt.official.domain.auth.model.UserPhoneNumber
-
-fun UserPhoneNumber.toRequest(): FindAccountRequest =
-    FindAccountRequest(
-        phone = phone
-    )
 
 fun FindAccountResponse.toAuthDomain(): Auth =
     Auth(
