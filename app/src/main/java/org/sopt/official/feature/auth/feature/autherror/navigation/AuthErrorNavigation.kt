4472da8ce42a7d584ba3859926868ce19a4688f7
LEE YOU BIN <youbeene3409@naver.com>
2025-06-17 02:41:12 +0900

    [feat/#1178] 에러화면 네비게이션 구현

---
+++ app/src/main/java/org/sopt/official/feature/auth/feature/autherror/navigation/AuthErrorNavigation.kt
@@ -0,0 +1,30 @@
+package org.sopt.official.feature.auth.feature.autherror.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+import org.sopt.official.feature.auth.feature.autherror.AuthErrorScreen
+
+fun NavController.navigateAuthError(
+    navOptions: NavOptions? = null,
+) {
+    navigate(
+        route = AuthErrorNavigation,
+        navOptions = navOptions,
+    )
+}
+
+fun NavGraphBuilder.authErrorNavGraph(
+    onRetryClick: () -> Unit,
+) {
+    composable<AuthErrorNavigation> {
+        AuthErrorScreen(
+            onRetryClick = onRetryClick,
+        )
+    }
+}
+
+@Serializable
+data object AuthErrorNavigation
