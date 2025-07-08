50a7ca43d30fd2455a6322a153a18419c2c9c42a
Hyobeen-Park <amanda0109@naver.com>
2025-07-08 22:48:12 +0900

    [FEAT/#1189] 최근게시물 인디케이터, 제목세션 구현

--- feature/home/src/main/java/org/sopt/official/feature/home/component/HomeLatestNewsSection.kt
+++ feature/home/src/main/java/org/sopt/official/feature/home/component/HomeLatestNewsSection.kt
@@ -1,22 +1,34 @@
 package org.sopt.official.feature.home.component
 
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.feature.home.R
 import org.sopt.official.feature.home.model.HomePlaygroundEmptyPostModel
 import org.sopt.official.feature.home.model.HomePlaygroundPostModel
 
@@ -63,15 +75,14 @@ internal fun HomeLatestNewsSection(
         }
     }
 
-    Column {
-        Row {
-            Text(
-                text = "최신 게시물",
-                style = SoptTheme.typography.heading20B,
-                color = SoptTheme.colors.primary
-            )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        TitleSection(onClick = navigateToPlayground)
 
-        }
+        Spacer(modifier = Modifier.height(16.dp))
+
         HorizontalPager(
             state = pagerState,
             beyondViewportPageCount = 1,
@@ -103,6 +114,51 @@ internal fun HomeLatestNewsSection(
                     )
                 }
             }
+        }
+
+        Spacer(modifier = Modifier.height(18.dp))
+
+        HomePageIndicator(
+            numberOfPages = pageCount,
+            selectedPage = pagerState.currentPage
+        )
+    }
+}
+
+@Composable
+private fun TitleSection(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = "최신 게시물",
+            style = SoptTheme.typography.heading20B,
+            color = SoptTheme.colors.primary
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(onClick = onClick)
+        ) {
+            Text(
+                text = "전체보기",
+                style = SoptTheme.typography.label12SB,
+                color = SoptTheme.colors.onSurface300
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_right),
+                contentDescription = null,
+                tint = SoptTheme.colors.onSurface300,
+                modifier = Modifier
+                    .size(16.dp)
+            )
         }
     }
 }
