11ff222b1b5876e4264e9c14b4a24c41c23b822d
Hyobeen-Park <amanda0109@naver.com>
2025-07-22 20:47:29 +0900

    [DEL/#1189] HomePlaygroundEmptyPostModel 삭제

--- feature/home/src/main/java/org/sopt/official/feature/home/model/HomePlaygroundPostModel.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.official.feature.home.model
-
-data class HomePlaygroundEmptyPostModel(
-    val category: String,
-    val description: String,
-    val iconUrl: String,
-    val webLink: String
-)
