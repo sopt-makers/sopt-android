a810444eb8fe9e76e934dc1ce92b0a0dea66208a
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-08-01 17:22:18 +0900

    chore: replace preview spec

--- feature/soptamp/src/main/java/org/sopt/official/stamp/util/DefaultPreview.kt
+++ feature/soptamp/src/main/java/org/sopt/official/stamp/util/DefaultPreview.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
     name = "phone",
-    device = "spec:shape=Normal,width=360,height=800,unit=dp,dpi=480",
+    device = "spec:width=360dp,height=800dp,dpi=480",
     showBackground = true,
     backgroundColor = 0xFFFFFF,
 )
