74fcde3178e2585600e24443e502f0234ca5308b
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-08-02 22:04:53 +0900

    config: feature plugin 분리

--- build-logic/convention/src/main/kotlin/org/sopt/official/plugin/DeeplinkPlugin.kt
+++ build-logic/convention/src/main/kotlin/org/sopt/official/plugin/DeeplinkPlugin.kt
@@ -1,18 +1,13 @@
 package org.sopt.official.plugin
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
+import org.sopt.official.plugin.base.BasePlugin
 
-class DeeplinkPlugin : Plugin<Project> {
+class DeeplinkPlugin : BasePlugin() {
     override fun apply(target: Project) = with(target) {
-        with(plugins) {
-            apply("com.google.devtools.ksp")
-        }
+        applyPlugin("com.google.devtools.ksp")
 
-        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
         dependencies {
             "implementation"(libs.findLibrary("deeplink.dispatch").get())
             "ksp"(libs.findLibrary("deeplink.dispatch.processor").get())
--- build-logic/convention/src/main/kotlin/org/sopt/official/plugin/KotlinJvmPlugin.kt
+++ build-logic/convention/src/main/kotlin/org/sopt/official/plugin/KotlinJvmPlugin.kt
@@ -1,16 +1,14 @@
 package org.sopt.official.plugin
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.sopt.official.plugin.base.BasePlugin
 
-class KotlinJvmPlugin : Plugin<Project> {
+class KotlinJvmPlugin : BasePlugin() {
     override fun apply(target: Project): Unit = with(target) {
-        with(plugins) {
-            apply("org.jetbrains.kotlin.jvm")
-            apply("java-library")
-        }
+        applyPlugin("org.jetbrains.kotlin.jvm")
+        applyPlugin("java-library")
 
         tasks.withType(KotlinCompile::class.java).configureEach {
             compilerOptions {
