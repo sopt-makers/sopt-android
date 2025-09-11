4fad047d0d84bdf2088daf779a6c34c0294d6c6b
seongmin.kim <arcn1345@gmail.com>
2025-09-11 22:53:48 +0900

    mod: successOr 함수 수정

--- domain/home/src/main/java/org/sopt/official/domain/home/result/Result.kt
+++ domain/home/src/main/java/org/sopt/official/domain/home/result/Result.kt
@@ -24,17 +24,4 @@
  */
 package org.sopt.official.domain.home.result
 
-sealed class Result<out R> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: Throwable) : Result<Nothing>()
-}
-
-fun <T> Result<T>.successOr(fallback: T): T {
-    return (this as? Result.Success<T>)?.data ?: fallback
-}
-
-inline fun <T, R> T.runCatching(block: T.() -> R): Result<R> = try {
-    Result.Success(block())
-} catch (e: Throwable) {
-    Result.Error(e)
-}
+fun <T> Result<T>.successOr(fallback: T): T = this.getOrElse { fallback }
