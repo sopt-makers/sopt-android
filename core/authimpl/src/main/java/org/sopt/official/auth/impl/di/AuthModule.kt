de981f91b9e07ee1d4c34079afa6e8cdc00004ab
Hyobeen-Park <amanda0109@naver.com>
2025-09-20 08:19:00 +0900

    [MOD/#1333] 앱 진입 시 토큰 갱신 시도

--- core/authimpl/src/main/java/org/sopt/official/auth/impl/di/AuthModule.kt
+++ core/authimpl/src/main/java/org/sopt/official/auth/impl/di/AuthModule.kt
@@ -34,9 +34,11 @@ import org.sopt.official.auth.impl.api.AuthService
 import org.sopt.official.auth.impl.local.DefaultLocalAuthDataSource
 import org.sopt.official.auth.impl.remote.DefaultRemoteAuthDataSource
 import org.sopt.official.auth.impl.repository.AuthRepositoryImpl
+import org.sopt.official.auth.impl.repository.DefaultCentralizeAuthRepository
 import org.sopt.official.auth.impl.source.LocalAuthDataSource
 import org.sopt.official.auth.impl.source.RemoteAuthDataSource
 import org.sopt.official.auth.repository.AuthRepository
+import org.sopt.official.auth.repository.CentralizeAuthRepository
 import org.sopt.official.common.di.AppRetrofit
 import org.sopt.official.common.di.Auth
 import retrofit2.Retrofit
@@ -55,6 +57,10 @@ object AuthModule {
         @Binds
         @Singleton
         fun bindAuthRepository(repository: AuthRepositoryImpl): AuthRepository
+
+        @Binds
+        @Singleton
+        fun bindCentralizeAuthRepository(repository: DefaultCentralizeAuthRepository): CentralizeAuthRepository
 
         @Binds
         @Singleton
