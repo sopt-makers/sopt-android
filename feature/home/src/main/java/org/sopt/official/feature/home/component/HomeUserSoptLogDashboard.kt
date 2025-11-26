897eea82ad0caa00ea37f16bac4ac90525906816
sonms <smsms5676@naver.com>
2025-11-27 01:10:37 +0900

    [mod/#1395] 홈 대시보드에 플그 프로필 이동 및 띄우기 구현

--- feature/home/src/main/java/org/sopt/official/feature/home/component/HomeUserSoptLogDashboard.kt
+++ feature/home/src/main/java/org/sopt/official/feature/home/component/HomeUserSoptLogDashboard.kt
@@ -31,13 +31,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color.Companion.Unspecified
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -47,12 +51,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import org.sopt.official.designsystem.Black40
 import org.sopt.official.designsystem.SoptTheme
 import org.sopt.official.designsystem.SoptTheme.colors
 import org.sopt.official.designsystem.SoptTheme.typography
 import org.sopt.official.designsystem.SuitBold
-import org.sopt.official.feature.home.R.drawable.ic_soptlog
+import org.sopt.official.feature.home.R.drawable.ic_edit_profile
 import org.sopt.official.feature.home.model.HomeUserSoptLogDashboardModel
 
 @Composable
@@ -130,11 +135,23 @@ internal fun HomeUserSoptLogDashboardForMember(
                     HomeGenerationChips(homeUserSoptLogDashboardModel = homeUserSoptLogDashboardModel)
                 }
                 Spacer(modifier = Modifier.weight(weight = 1f))
-                Icon(
-                    imageVector = ImageVector.vectorResource(ic_soptlog),
-                    contentDescription = null,
-                    tint = Unspecified,
-                )
+
+                if (homeUserSoptLogDashboardModel.userProfile.isNotEmpty()) {
+                    AsyncImage(
+                        model = homeUserSoptLogDashboardModel.userProfile,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(54.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop,
+                    )
+                } else {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(ic_edit_profile),
+                        contentDescription = null,
+                        tint = Unspecified,
+                    )
+                }
             }
         }
     )
