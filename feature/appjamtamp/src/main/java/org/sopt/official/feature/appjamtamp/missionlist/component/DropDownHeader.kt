e86c75d0467680c0f316f252a60b63c3948b8213
Hyobeen-Park <amanda0109@naver.com>
2025-12-18 00:21:59 +0900

    [FEAT/#1434] 백버튼 헤더

--- feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/missionlist/component/DropDownHeader.kt
+++ feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/missionlist/component/DropDownHeader.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -45,7 +46,7 @@ internal fun DropDownHeader(
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
+        modifier = modifier.fillMaxWidth()
     ) {
         Text(
             text = title,
