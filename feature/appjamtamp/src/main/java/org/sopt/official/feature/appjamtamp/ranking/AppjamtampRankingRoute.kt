61bceaa59fb2dfc95fe36d2804a9f35deebbf968
seongmin.kim <arcn1345@gmail.com>
2025-12-23 00:16:31 +0900

    feat: 앱잼팀 랭킹 뷰 구현

---
+++ feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/ranking/AppjamtampRankingRoute.kt
@@ -0,0 +1,10 @@
+package org.sopt.official.feature.appjamtamp.ranking
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun AppjamtampRankingRoute(
+
+) {
+    AppjamtampRankingScreen()
+}
