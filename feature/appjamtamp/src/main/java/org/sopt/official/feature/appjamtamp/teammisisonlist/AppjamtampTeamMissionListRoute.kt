5ff3f6b2f66e119e8e60b050ff5dea4d8c7ccc26
seongmin.kim <arcn1345@gmail.com>
2025-12-23 00:57:20 +0900

    feat: 앱잼팀 미션 뷰 구현

---
+++ feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/teammisisonlist/AppjamtampTeamMissionListRoute.kt
@@ -0,0 +1,8 @@
+package org.sopt.official.feature.appjamtamp.teammisisonlist
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun AppjamtampTeamMissionListRoute() {
+    AppjamtampTeamMissionListScreen()
+}
