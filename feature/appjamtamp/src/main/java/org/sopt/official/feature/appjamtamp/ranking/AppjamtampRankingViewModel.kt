a960fed51cd02d10182c0b3d78a43f9e6cccf5f7
seongmin.kim <arcn1345@gmail.com>
2026-01-04 14:22:52 +0900

    feat: 앱잽탬프 팀 랭킹 뷰모델 추가

---
+++ feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/ranking/AppjamtampRankingViewModel.kt
@@ -0,0 +1,44 @@
+package org.sopt.official.feature.appjamtamp.ranking
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.sopt.official.domain.appjamtamp.repository.AppjamtampRepository
+import org.sopt.official.feature.appjamtamp.ranking.model.AppjamtampRankingState
+import org.sopt.official.feature.appjamtamp.ranking.model.toUiModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AppjamtampRankingViewModel @Inject constructor(
+    private val appjamtampRepository: AppjamtampRepository
+) : ViewModel() {
+
+    private val _state: MutableStateFlow<AppjamtampRankingState> = MutableStateFlow(AppjamtampRankingState.Loading)
+    val state: StateFlow<AppjamtampRankingState> = _state.asStateFlow()
+
+    fun getRankingData() {
+        viewModelScope.launch {
+            _state.value = AppjamtampRankingState.Loading
+
+            val top10Deferred = async { appjamtampRepository.getAppjamtampMissionRanking(size = 12) }
+            val top3Deferred = async { appjamtampRepository.getAppjamtampMissionTop3(size = 3) }
+
+            val top10Result = top10Deferred.await()
+            val top3Result = top3Deferred.await()
+
+            if (top10Result.isSuccess && top3Result.isSuccess) {
+                _state.value = AppjamtampRankingState.Success(
+                    top3RecentRankingListUiModel = top3Result.getOrThrow().toUiModel(),
+                    top10MissionScoreListUiModel = top10Result.getOrThrow().toUiModel()
+                )
+            } else {
+                _state.value = AppjamtampRankingState.Failure
+            }
+        }
+    }
+}
