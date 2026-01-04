176cb253d0f8f28e39600b3b22944726415888a0
seongmin.kim <arcn1345@gmail.com>
2026-01-04 14:19:35 +0900

    feat: 서버통신 모듈 생성

---
+++ data/appjamtamp/src/main/java/org/sopt/official/data/appjamtamp/di/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package org.sopt.official.data.appjamtamp.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.official.data.appjamtamp.repository.AppjamtampRepositoryImpl
+import org.sopt.official.domain.appjamtamp.repository.AppjamtampRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAppjamtampRepository(
+        appjamtampRepositoryImpl: AppjamtampRepositoryImpl
+    ): AppjamtampRepository
+
+
+}
