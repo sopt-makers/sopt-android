76e9ae4d886313f0834ed71d25298f72aedc393b
Hyobeen-Park <amanda0109@naver.com>
2026-01-07 20:47:19 +0900

    [MOD/#1457] sideEffect 수정

--- feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/missiondetail/MissionDetailRoute.kt
+++ feature/appjamtamp/src/main/java/org/sopt/official/feature/appjamtamp/missiondetail/MissionDetailRoute.kt
@@ -29,7 +29,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.flowWithLifecycle
 import org.sopt.official.designsystem.SoptTheme
 import org.sopt.official.designsystem.component.dialog.TwoButtonDialog
 import org.sopt.official.feature.appjamtamp.component.AppjamtampButton
@@ -68,15 +68,12 @@ internal fun MissionDetailRoute(
     }
 
     LaunchedEffect(viewModel.sideEffect, lifecycleOwner) {
-        lifecycleOwner.lifecycle.repeatOnLifecycle(
-            Lifecycle.State.STARTED
-        ) {
-            viewModel.sideEffect.collect { sideEffect ->
+        viewModel.sideEffect.flowWithLifecycle(lifecycleOwner.lifecycle)
+            .collect { sideEffect ->
                 when (sideEffect) {
                     is MissionDetailSideEffect.NavigateUp -> navigateUp()
                 }
             }
-        }
     }
 
     if (uiState.viewType == DetailViewType.WRITE) {
