063b9e39f0dd00136c0b21074285df36edb0a007
hanminjae <hanminjae131@gmail.com>
2026-01-17 20:23:37 +0900

    feat(core): Define AppScope and AppGraph for Metro DI
    - Create AppScope in core:common
    - Create AppGraph in :app extending MetroAppComponentProviders and ViewModelGraph

---
+++ app/src/main/java/org/sopt/official/di/AppGraph.kt
@@ -0,0 +1,9 @@
+package org.sopt.official.di
+
+import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.MetroAppComponentProviders
+import dev.zacsweers.metro.ViewModelGraph
+import org.sopt.official.common.di.AppScope
+
+@DependencyGraph(AppScope::class)
+interface AppGraph : MetroAppComponentProviders, ViewModelGraph
---
+++ core/common/src/main/java/org/sopt/official/common/di/AppScope.kt
@@ -0,0 +1,3 @@
+package org.sopt.official.common.di
+
+abstract class AppScope private constructor()
