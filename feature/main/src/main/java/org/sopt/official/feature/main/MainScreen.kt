71af7fc2380d1d1fbc9f784f6ff7045ec770ea9e
sonms <smsms5676@naver.com>
2026-04-18 23:00:42 +0900

    [REFRACTOR#1560] 사용하지 않는 import 제거

--- feature/main/src/main/java/org/sopt/official/feature/main/MainScreen.kt
+++ feature/main/src/main/java/org/sopt/official/feature/main/MainScreen.kt
@@ -76,7 +76,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.zIndex
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavDestination.Companion.hasRoute
