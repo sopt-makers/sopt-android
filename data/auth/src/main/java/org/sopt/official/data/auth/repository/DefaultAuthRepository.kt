056afe069601c95e47ac93f062375b598f1a4369
seungjun <jjw5741@naver.com>
2026-05-05 23:29:47 +0900

    spotlessApply

--- data/auth/src/main/java/org/sopt/official/data/auth/repository/DefaultAuthRepository.kt
+++ data/auth/src/main/java/org/sopt/official/data/auth/repository/DefaultAuthRepository.kt
@@ -1,6 +1,6 @@
 /*
  * MIT License
- * Copyright 2025 SOPT - Shout Our Passion Together
+ * Copyright 2025-2026 SOPT - Shout Our Passion Together
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
