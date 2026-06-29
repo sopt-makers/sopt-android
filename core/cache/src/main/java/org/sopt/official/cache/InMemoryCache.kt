2524a4b5e4385593b38c988c0435d1a43ea26d9d
sonms <smsms5676@naver.com>
2026-06-29 21:23:40 +0900

    [FEAT#1603] core:cache 모듈 추가 및 InMemoryCache 구현
    - `core:cache` 모듈 신규 생성 및 Kotlin JVM 환경 설정
    - `InMemoryCache<T>` 클래스 구현을 통해 스레드 세이프한 인메모리 캐싱 기능 제공
    - `Mutex`를 사용하여 데이터 fetch 시 동시성 제어 및 중복 호출 방지
    - `StateFlow`를 통해 캐시된 데이터의 상태를 관찰할 수 있도록 구현
    - 캐시 초기화를 위한 `invalidate` 함수 추가

---
+++ core/cache/src/main/java/org/sopt/official/cache/InMemoryCache.kt
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ * Copyright 2026 SOPT - Shout Our Passion Together
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.sopt.official.cache
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+class InMemoryCache<T> {
+    private val mutex = Mutex()
+    private val _data = MutableStateFlow<T?>(null)
+    val data: StateFlow<T?> = _data.asStateFlow()
+
+    suspend fun getOrFetch(fetcher: suspend () -> T): T {
+        _data.value?.let { return it }
+        return mutex.withLock {
+            _data.value ?: fetcher().also { _data.value = it }
+        }
+    }
+
+    suspend fun invalidate() {
+        mutex.withLock { _data.value = null }
+    }
+}
