50c320d68ba967f8d09de9891cd1b005a681e80b
Son Juwan <wndhks171@naver.com>
2026-07-02 21:28:01 +0900

    [FEAT/#1607] 솝레터 작성 api

---
+++ data/sopletter/src/main/java/org/sopt/official/data/sopletter/api/SopletterWriteApi.kt
@@ -0,0 +1,15 @@
+package org.sopt.official.data.sopletter.api
+
+import org.sopt.official.data.sopletter.dto.SopletterWriteRequest
+import org.sopt.official.data.sopletter.dto.SopletterWriteResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface SopletterService {
+    @POST("sopt-letter/topics/{topicId}/messages")
+    suspend fun postTopicSopletter(
+        @Path("topicId") topicId: Long,
+        @Body request: SopletterWriteRequest
+    ): SopletterWriteResponse
+}
---
+++ domain/sopletter/src/main/java/org/sopt/official/sopletter/model/SopletterWriteModel.kt
@@ -0,0 +1,17 @@
+package org.sopt.official.sopletter.model
+
+
+data class Sopletter(
+    val messageId: Long,
+    val topicId: Long?,
+    val authorNickname: String,
+    val content: String,
+    val colorCode: String,
+    val rotationDegree: Double,
+    val shapeType: String,
+    val createdAt: String,
+    val updatedAt: String,
+    val likeCount: Int,
+    val likedByMe: Boolean,
+    val mine: Boolean
+)
