db45968edf0dc4c1be80915194f2321f643893f9
Son Juwan <wndhks171@naver.com>
2026-07-02 21:26:21 +0900

    [FEAT/#1607] 솝레터 작성 model

--- feature/sopletter/src/main/java/org/sopt/official/feature/sopletter/write/model/SopletterWriteState.kt
+++ feature/sopletter/src/main/java/org/sopt/official/feature/sopletter/write/model/SopletterWriteState.kt
@@ -5,9 +5,27 @@ import androidx.compose.runtime.Immutable
 @Immutable
 data class SopletterWriteUiState(
     val writerName: String = "",
-
     val isLoading: Boolean = false,
-    val isSuccess: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String? = null
 )
+
+sealed interface SopletterWriteSideEffect {
+    data class ShowSnackbar(
+        val message: String,
+        val type: SopletterSnackbarType
+    ) : SopletterWriteSideEffect
+
+    data object NavigateToMain : SopletterWriteSideEffect
+}
+
+
+// TODO: 테스트용, merge 시점에 삭제 예정
+interface SopletterSnackbarType{
+    @Immutable
+    data object SUCCESS : SopletterSnackbarType
+    @Immutable
+    data object FAILURE : SopletterSnackbarType
+    @Immutable
+    data object WARNING : SopletterSnackbarType
+}
