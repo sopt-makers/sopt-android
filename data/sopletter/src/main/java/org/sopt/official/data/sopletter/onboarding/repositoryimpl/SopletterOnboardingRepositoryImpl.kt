1bf635808d672aadc8669abcfa1d51c13d5132a4
sonms <smsms5676@naver.com>
2026-07-04 02:27:46 +0900

    [REFACTOR#1613] 코리 수정

--- data/sopletter/src/main/java/org/sopt/official/data/sopletter/onboarding/repositoryimpl/SopletterOnboardingRepositoryImpl.kt
+++ data/sopletter/src/main/java/org/sopt/official/data/sopletter/onboarding/repositoryimpl/SopletterOnboardingRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.sopt.official.data.sopletter.onboarding.repositoryimpl
 
+import org.sopt.official.common.coroutines.suspendRunCatching
 import org.sopt.official.data.sopletter.onboarding.service.SopletterOnboardingService
 import org.sopt.official.domain.sopletter.onboarding.model.SopletterOnboardingModel
 import org.sopt.official.domain.sopletter.onboarding.repository.SopletterOnboardingRepository
@@ -9,11 +10,11 @@ internal class SopletterOnboardingRepositoryImpl @Inject constructor(
     private val sopletterOnboardingService: SopletterOnboardingService,
 ) : SopletterOnboardingRepository {
 
-    override suspend fun getOnboarding(): Result<SopletterOnboardingModel> = runCatching {
+    override suspend fun getOnboarding(): Result<SopletterOnboardingModel> = suspendRunCatching {
         sopletterOnboardingService.getOnboarding().toDomain()
     }
 
-    override suspend fun completeOnboarding(): Result<SopletterOnboardingModel> = runCatching {
+    override suspend fun completeOnboarding(): Result<SopletterOnboardingModel> = suspendRunCatching {
         sopletterOnboardingService.completeOnboarding().toDomain()
     }
 }
