ae16cb3252082783abaa384ce2c3eb16e7bd6182
Son Juwan <wndhks171@naver.com>
2026-07-12 21:22:32 +0900

    [FEAT/#1629] soptlog amplitude eventtype

---
+++ feature/soptlog/src/main/java/SoptlogAnalytics.kt
@@ -0,0 +1,9 @@
+import org.sopt.official.analytics.AnalyticsEvent
+import org.sopt.official.analytics.EventType
+
+enum class SoptlogAnalyticsEvent(
+    override val type : EventType,
+    override val eventName : String
+):AnalyticsEvent {
+    VIEW_SOPTLOG_MAIN(EventType.VIEW,"soptlog_main"),
+}
