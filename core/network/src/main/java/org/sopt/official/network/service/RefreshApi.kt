777a6d6d2470b65575341f22632758b6899ecea4
seungjun <jjw5741@naver.com>
2026-09-12 10:23:09 +0900

    [REFACTOR/#1658] 인증, 출석 응답 래퍼를 BaseResponse로 통일

--- core/network/src/main/java/org/sopt/official/network/service/RefreshApi.kt
+++ core/network/src/main/java/org/sopt/official/network/service/RefreshApi.kt
@@ -25,14 +25,14 @@
 package org.sopt.official.network.service
 
 import org.sopt.official.network.model.request.ExpiredTokenRequest
-import org.sopt.official.network.model.response.NullableBaseAuthResponse
-import org.sopt.official.network.model.response.ValidTokenResponse
+import org.sopt.official.network.model.response.RefreshTokenResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
+import org.sopt.official.common.network.BaseResponse
 
 interface RefreshApi {
     @POST("/api/v1/auth/refresh/app")
     suspend fun refreshToken(
         @Body request: ExpiredTokenRequest,
-    ): NullableBaseAuthResponse<ValidTokenResponse>
+    ): BaseResponse<RefreshTokenResponse?>
 }
