2ece4e8772040c79e39063279fb0163e634707aa
Hyobeen Park <98209004+Hyobeen-Park@users.noreply.github.com>
2026-09-24 23:50:24 +0900

    [REFACTOR/#1656] 알림 화면에 mds 적용 (#1662)
    * [ADD/#1656] 알림 액티비티에 SoptTheme 추가
    
    * [REFACTOR/#1656] 컴포넌트에 mds 적용
    
    * [DEL/#1656] delete ErrorSnackBar
    
    * [refactor/#1656] apply mds in NotificationActivity
    
    * [refactor/#1656] apply mds in NotificationDetailActivity
    
    * [refactor/#1656] apply mds in LoadingIndicator & provide SoptTheme to AuthActivity
    
    * [del/#1656] delete icons in notification module
    
    * Revert "[del/#1656] delete icons in notification module"
    
    This reverts commit da4281961109fa895062e75fc87fc2c0a9d3050b.
    
    * [del/#1656] delete icons in notification module
    
    * [del/#1656] delete NetworkErrorDialog in NotificationActivity
    
    * [fix/#1656] specify MdsDialogType
    
    * [MOD/#1656] modify NetworkErrorDialog

--- feature/notification/src/main/java/org/sopt/official/feature/notification/all/component/NotificationCategoryChip.kt
+++ feature/notification/src/main/java/org/sopt/official/feature/notification/all/component/NotificationCategoryChip.kt
@@ -24,17 +24,11 @@
  */
 package org.sopt.official.feature.notification.all.component
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import org.sopt.official.designsystem.SoptTheme
+import org.sopt.official.mds.components.chip.MdsChip
+import org.sopt.official.mds.components.chip.MdsChipSize
+import org.sopt.official.mds.components.chip.MdsChipType
 
 @Composable
 internal fun NotificationCategoryChip(
@@ -43,23 +37,12 @@ internal fun NotificationCategoryChip(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Text(
+    MdsChip(
         text = category,
-        color = if (isSelected) SoptTheme.colors.surface else SoptTheme.colors.onSurface300,
-        style = SoptTheme.typography.label14SB,
+        selected = isSelected,
+        onClick = onClick,
+        type = MdsChipType.SOLID,
+        size = MdsChipSize.SMALL,
         modifier = modifier
-            .background(
-                color = if (isSelected) SoptTheme.colors.onSurface700 else SoptTheme.colors.onSurface800,
-                shape = CircleShape
-            )
-            .border(
-                border = BorderStroke(
-                    width = 1.dp,
-                    color = if (isSelected) SoptTheme.colors.onSurface100 else SoptTheme.colors.onSurface700
-                ),
-                shape = CircleShape
-            )
-            .clickable(onClick = onClick)
-            .padding(vertical = 9.dp, horizontal = 14.dp)
     )
 }
